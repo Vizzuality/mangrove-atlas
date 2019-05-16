8203cd119ed21568f83d1b9ee1857028852f892d
Migsar Navarro <migsar.navarro@vizzuality.com>
2019-05-16 15:38:53 +0100

    feat(NA): Add redux-first-router.

--- src/config/store.js
+++ src/config/store.js
@@ -1,13 +1,30 @@
-import { createStore, combineReducers } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { handleModule } from 'vizzuality-redux-tools';
 
 import * as pages from 'modules/pages';
+import router from './router';
+
+const {
+  initialDispatch,
+  reducer: routerReducer,
+  middleware: routerMiddleware,
+  enhancer: routerEnhancer
+} = router;
 
 const reducers = combineReducers({
+  router: routerReducer,
   page: handleModule(pages)
 });
 
-const store = createStore(reducers, devToolsEnhancer());
+const middleware = applyMiddleware(
+  routerMiddleware
+);
+
+const enhancers = composeWithDevTools(routerEnhancer, middleware);
+
+const store = createStore(reducers, enhancers);
+
+initialDispatch();
 
 export default store;
