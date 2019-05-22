dbb5ed3a56a074f412468ee7b00198e535d04c35
Migsar Navarro <migsar.navarro@vizzuality.com>
2019-05-22 16:26:32 +0100

    feat(#5): Add state to url.

--- src/config/store.js
+++ src/config/store.js
@@ -2,9 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { handleModule } from 'vizzuality-redux-tools';
 
+import { PAGES } from 'modules/pages/constants';
 import * as pages from 'modules/pages';
 import * as map from 'modules/map';
+import { queryState } from 'modules/query-state';
 import router from './router';
+
+queryState.config({
+  routerActions: PAGES.map(p => p.name)
+});
 
 const modules = [
   { namespace: 'page', components: pages },
@@ -18,13 +24,18 @@ const {
   enhancer: routerEnhancer
 } = router;
 
+const {
+  middleware: queryStateMiddleware
+} = queryState;
+
 const reducers = combineReducers({
   router: routerReducer,
   ...modules.reduce((acc, module) => ({...acc, [module.namespace]: handleModule(module.components)}), {})
 });
 
 const middleware = applyMiddleware(
-  routerMiddleware
+  routerMiddleware,
+  queryStateMiddleware
 );
 
 const enhancers = composeWithDevTools(routerEnhancer, middleware);
