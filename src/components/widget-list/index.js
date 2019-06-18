98287b1df2d8c8c5c8c73e322ce2a5333edd4e30
David Inga <david.inga@vizzuality.com>
2019-06-18 13:49:00 +0200

    connecting widgets and collapse

--- src/components/widget-list/index.js
+++ src/components/widget-list/index.js
@@ -1,18 +1,21 @@
 import { connect } from 'react-redux';
 import { fetchDashboards } from 'modules/dashboards/actions';
-import { fetchWidgets } from 'modules/widgets/actions';
+import { fetchWidgets, expandAll, collapseAll } from 'modules/widgets/actions';
 import { dashboardWidgets } from 'modules/widgets/selectors';
 
 import Component from './component';
 
 // todo: widgets are processed here so content can get customized
 const mapStateToProps = state => ({
+  isCollapsed: state.widgets.isCollapsed,
   widgets: dashboardWidgets(state)
 });
 
 const mapDispatchToProps = {
   fetchDashboards,
-  fetchWidgets
+  fetchWidgets,
+  collapseAll,
+  expandAll
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Component);
--- src/modules/widgets/reducers.js
+++ src/modules/widgets/reducers.js
@@ -1,4 +1,7 @@
-import { fetchRequested, fetchSucceeded, fetchFailed } from './actions';
+import {
+  fetchRequested, fetchSucceeded, fetchFailed,
+  collapseAll, expandAll, toggleCollapse
+} from './actions';
 
 export default {
   [fetchRequested]: state => ({
@@ -15,5 +18,28 @@ export default {
     ...state,
     isLoading: false,
     error: payload
-  })
+  }),
+  [collapseAll]: state => ({
+    ...state,
+    isCollapsed: true,
+    list: state.list.map(item => ({ ...item, isCollapsed: true }))
+  }),
+  [expandAll]: state => ({
+    ...state,
+    isCollapsed: false,
+    list: state.list.map(item => ({ ...item, isCollapsed: false }))
+  }),
+  [toggleCollapse]: (state, { payload }) => {
+    const list = state.list.map((item) => {
+      if (item.id !== payload.id) return item;
+      return ({ ...item, isCollapsed: !item.isCollapsed });
+    });
+    const noCollapsed = list.find(item => !item.isCollapsed);
+
+    return {
+      ...state,
+      list,
+      isCollapsed: !!noCollapsed
+    };
+  }
 };
