37679dd373c44394ba7d75bb1583daa3c78e6530
David Inga <david.inga@vizzuality.com>
2019-06-18 16:31:26 +0200

    disabled url update

--- src/components/map/component.js
+++ src/components/map/component.js
@@ -1,160 +1,76 @@
 import React, { Component } from 'react';
-import ReactMapGL, { FlyToInterpolator } from 'react-map-gl';
+import MapGL, { FlyToInterpolator } from 'react-map-gl';
 import PropTypes from 'prop-types';
-
 import isEqual from 'lodash/isEqual';
 import isEmpty from 'lodash/isEmpty';
+import styles from './style.module.scss';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 class Map extends Component {
   static propTypes = {
-    children: PropTypes.func,
     viewport: PropTypes.shape({}),
-    bounds: PropTypes.shape({
-      bbox: PropTypes.array,
-      options: PropTypes.shape({})
-    }),
-    dragPan: PropTypes.bool,
-    dragRotate: PropTypes.bool, // A boolean that allows rotating
-    setMapViewport: PropTypes.func,
-    onViewportChange: PropTypes.func,
-    onLoad: PropTypes.func,
-    onStyleLoad: PropTypes.func
+    setMapViewport: PropTypes.func
   }
 
   static defaultProps = {
-    children: null,
-    viewport: { zoom: 2, lat: 0, lng: 0 },
-    bounds: {},
-    dragPan: true,
-    dragRotate: true,
-    onViewportChange: () => {},
-    onLoad: () => {},
-    onStyleLoad: () => {},
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      longitude: 0,
+      latitude: 0,
+      zoom: 2,
+      maxZoom: 16,
+      bearing: 0,
+      pitch: 0
+    },
     setMapViewport: () => {}
   }
 
+  // constructor(props) {
+  //   super(props);
+  //   const { viewport } = props;
+  //   this.state = { viewport };
+  // }
+
   componentDidMount() {
-    const { bounds } = this.props;
-
-    if (!isEmpty(bounds) && !!bounds.bbox) {
-      this.fitBounds();
-    }
+    window.addEventListener('resize', this.resize);
+    this.resize();
   }
 
-  componentDidUpdate(prevProps) {
-    const { bounds: prevBounds } = prevProps;
-    const { bounds } = this.props;
+  onViewportChange = (nextViewport) => {
+    const { viewport, setMapViewport } = this.props;
 
-    if (!isEmpty(bounds) && !isEqual(bounds, prevBounds)) {
-      this.fitBounds();
-    }
+    setMapViewport({ ...viewport, ...nextViewport });
   }
 
-  setElement = (map) => {
-    this.map = map && map.getMap();
-  }
-
-  onLoad = () => {
-    const { onLoad, setMapLoaded, onStyleLoad } = this.props;
-    setMapLoaded(true);
-
-    this.map.on('style.load', onStyleLoad);
-
-    onLoad({
-      map: this.map
-    });
-  }
-
-  onResize = (resizeViewport) => {
-    const { viewport } = this.props;
+  resize = () => {
     this.onViewportChange({
-      ...viewport,
-      ...resizeViewport
+      width: window.innerWidth,
+      height: window.innerHeight
     });
   }
-
-  onMoveEnd = () => {
-    const { viewport, setMapFlying } = this.props;
-
-    if (this.map) {
-      const bearing = this.map.getBearing();
-      const pitch = this.map.getPitch();
-      const zoom = this.map.getZoom();
-      const { lng, lat } = this.map.getCenter();
-
-      const newViewport = {
-        ...viewport,
-        bearing,
-        pitch,
-        zoom,
-        latitude: lat,
-        longitude: lng
-      };
-
-      setMapFlying(false);
-      this.onViewportChange(newViewport);
-    }
-  }
-
-  onViewportChange = (viewport) => {
-    const { setMapViewport, onViewportChange } = this.props;
-
-    setMapViewport(viewport);
-    onViewportChange(viewport);
-  }
-
-  fitBounds = () => {
-    const { bounds, settings: { isFlying }, setMapFlying } = this.props;
-    const { bbox, options } = bounds;
-
-    if (isFlying) {
-      this.map.off('moveend', this.onMoveEnd);
-    }
-
-    setMapFlying(true);
-
-    requestAnimationFrame(() => {
-      this.map.fitBounds(
-        [[bbox[0], bbox[1]], [bbox[2], bbox[3]]],
-        options
-      );
-
-      this.map.once('moveend', this.onMoveEnd);
-    });
-  };
 
   render() {
     const {
-      className,
-      width,
-      height,
-      viewport,
-      settings: { isFlying, isLoaded },
-      children,
-      dragPan,
-      dragRotate,
-      ...mapboxProps
+      mapStyle,
+      mapboxApiAccessToken,
+      viewport
     } = this.props;
 
-    const mapProps = {
-      ...mapboxProps,
-      ...viewport,
-      height: height || viewport.height,
-      width: width || viewport.width,
-      dragPan: dragPan && !isFlying,
-      dragRotate: dragRotate && !isFlying,
-      transitionInterpolator: new FlyToInterpolator(),
-      onLoad: this.onLoad,
-      onResize: this.onResize,
-      onViewportChange: this.onViewportChange
-    };
-
     return (
-      <ReactMapGL className={className} ref={this.setElement} {...mapProps}>
-        {isLoaded && !!this.map && typeof children === 'function' && children(this.map)}
-      </ReactMapGL>
+      <MapGL
+        className={styles.map}
+        ref={this.setElement}
+        {...viewport}
+        mapStyle={mapStyle}
+        width="100%"
+        height="100%"
+        mapboxApiAccessToken={mapboxApiAccessToken}
+        onViewportChange={this.onViewportChange}
+      >
+        {/* {isLoaded && !!this.map && typeof children === 'function' && children(this.map)} */}
+      </MapGL>
     );
   }
 }
