1a7ba19c1e1befe8ab270a1932bd29ba92ac54f8
David Inga <david.inga@vizzuality.com>
2019-06-19 18:30:08 +0200

    changed thumbnails for basemap selector

--- src/components/basemap-selector/component.js
+++ src/components/basemap-selector/component.js
@@ -2,9 +2,9 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { basemaps } from './constants';
-import lightThumb from './thumbs/light.png';
-import darkThumb from './thumbs/dark.png';
-import satelliteThumb from './thumbs/satellite.jpeg';
+import lightThumb from './thumbs/btn-light@2x.png';
+import darkThumb from './thumbs/btn-dark@2x.png';
+import satelliteThumb from './thumbs/btn-satellite@2x.png';
 import styles from './style.module.scss';
 
 const thumbs = {
@@ -52,7 +52,7 @@ class BasemapSelector extends PureComponent {
                 [styles.selected]: currentBasemap.id === b.id
               })}
             >
-              <img src={thumbs[b.id]} alt={b.name} />
+              <img src={thumbs[b.id]} alt={b.name} width="35px" height="45px" />
             </button>
           ))}
         </div>
