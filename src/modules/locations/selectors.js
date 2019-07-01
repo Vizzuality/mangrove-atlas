5c3ca7cbb5341d20c11d0844604f9b8a51535c99
David Inga <david.inga@vizzuality.com>
2019-07-01 12:57:17 +0200

    fixed mangrove net change for wordwide

--- src/modules/locations/selectors.js
+++ src/modules/locations/selectors.js
@@ -11,20 +11,38 @@ export const currentLocation = createSelector(
     if (!_currentId) return null;
 
     if (_currentId.id === 'global') {
-      const countries = _locations.filter(location => location.type === 'admin0-eez');
+      const countries = _locations.filter(location => location.type === 'country');
       const globalData = {
         type: 'global',
         name: 'Worldwide',
         dashboardId: defaultDashboards.global,
         length_coast_m: 1634701000,
         length_mangrove_m: {
-          1996: sumBy(countries, c => c.length_mangrove_m['1996']),
-          2007: sumBy(countries, c => c.length_mangrove_m['2007']),
-          2008: sumBy(countries, c => c.length_mangrove_m['2008']),
-          2009: sumBy(countries, c => c.length_mangrove_m['2009']),
-          2010: sumBy(countries, c => c.length_mangrove_m['2010']),
-          2015: sumBy(countries, c => c.length_mangrove_m['2015']),
-          2016: sumBy(countries, c => c.length_mangrove_m['2016'])
+          1996: sumBy(countries, c => parseFloat(c.length_mangrove_m['1996'])),
+          2007: sumBy(countries, c => parseFloat(c.length_mangrove_m['2007'])),
+          2008: sumBy(countries, c => parseFloat(c.length_mangrove_m['2008'])),
+          2009: sumBy(countries, c => parseFloat(c.length_mangrove_m['2009'])),
+          2010: sumBy(countries, c => parseFloat(c.length_mangrove_m['2010'])),
+          2015: sumBy(countries, c => parseFloat(c.length_mangrove_m['2015'])),
+          2016: sumBy(countries, c => parseFloat(c.length_mangrove_m['2016']))
+        },
+        mangrove_loss_m2: {
+          1996: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['1996'])),
+          2007: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['2007'])),
+          2008: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['2008'])),
+          2009: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['2009'])),
+          2010: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['2010'])),
+          2015: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['2015'])),
+          2016: sumBy(countries, c => parseFloat(c.mangrove_loss_m2['2016']))
+        },
+        mangrove_gain_m2: {
+          1996: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['1996'])),
+          2007: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['2007'])),
+          2008: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['2008'])),
+          2009: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['2009'])),
+          2010: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['2010'])),
+          2015: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['2015'])),
+          2016: sumBy(countries, c => parseFloat(c.mangrove_gain_m2['2016']))
         }
       };
       return globalData;
