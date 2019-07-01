ed5fd587c9aae71e3e17391118aba85271c1f20f
David Inga <david.inga@vizzuality.com>
2019-07-01 13:46:59 +0200

    styles for select

--- src/components/select/component.js
+++ src/components/select/component.js
@@ -1,46 +1,18 @@
 import React, { PureComponent } from 'react';
 import ReactSelect from 'react-select';
-import styles from './style.module.scss';
-
-const theme = {
-  borderRadius: 0,
-  spacing: {
-    minHeight: 30 // line-height
-  }
-};
-
-const customStyles = {
-  container: provided => ({
-    ...provided,
-    display: 'inline-block'
-  }),
-  control: provided => ({
-    ...provided,
-    width: 'auto',
-    border: 0
-  }),
-  dummyInput: () => ({
-    width: 0
-  }),
-  input: provided => ({
-    ...provided,
-    position: 'absolute'
-  }),
-  singleValue: provided => ({
-    ...provided,
-    position: 'relative',
-    transform: 'none',
-    maxWidth: '100%',
-    margin: 0
-  }),
-  valueContainer: provided => ({
-    ...provided,
-    padding: 0
-  })
-};
+import { styles, theme } from './style';
 
 class Select extends PureComponent {
   state = { selectedOption: null }
+
+  options = {
+    isSearchable: false,
+    theme,
+    styles,
+    components: {
+      DropdownIndicator: null
+    }
+  }
 
   constructor(props) {
     super(props);
@@ -60,17 +32,12 @@ class Select extends PureComponent {
 
     return (
       <ReactSelect
-        className={styles.select}
-        value={selectedValue}
+        // className={stylesCSS.select}
+        {...this.options}
+        {...props}
         options={options}
         onChange={this.handleChange}
-        isSearchable={false}
-        theme={theme}
-        components={{
-          DropdownIndicator: null
-        }}
-        styles={customStyles}
-        {...props}
+        value={selectedValue}
       />
     );
   }
