67ee459d1f7184a05efead3d0c9f59219557d67a
David Inga <david.inga@icloud.com>
2019-07-02 12:57:36 +0200

    font size depending on title length

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -26,9 +26,14 @@ class Header extends PureComponent {
   render() {
     const { location } = this.props;
 
+    let stylesOverride = { fontSize: 60, lineHeight: 0.85 };
+
+    if (location && location.name.length > 10) stylesOverride = { fontSize: 45, lineHeight: 1 };
+    if (location && location.name.length > 30) stylesOverride = { fontSize: 30, lineHeight: 1 };
+
     return (
       <div className={styles.header}>
-        {location && <h1 className={styles.title}>{location.name}</h1>}
+        {location && <h1 className={styles.title} style={stylesOverride}>{location.name}</h1>}
         <button type="button" onClick={this.clickHandler} className={styles.searchButton}>
           <FontAwesomeIcon icon={faSearch} size="lg" />
         </button>
