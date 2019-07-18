c39782c5e2bf23921ff9584d8ad6f4e765ad43a7
maria <maluenarod@gmail.com>
2019-07-18 09:01:05 +0200

    button stories

--- src/components/button/component.js
+++ src/components/button/component.js
@@ -3,14 +3,25 @@ import classnames from 'classnames';
 import styles from './style.module.scss';
 
 export default (props) => {
-  const { children, isActive, isDisabled, isPrimary, isSecondary, ...domProps } = props;
+  const { children,
+    isActive,
+    isDisabled,
+    hasBackground,
+    isTransparent,
+    isGrey,
+    hasContrast,
+    ...domProps } = props;
 
   return (
     <button
       type="button"
       className={classnames(styles.button,
-        { [styles.primary]: isPrimary },
-        { [styles.secondary]: isSecondary })}
+        { [styles.background]: hasBackground },
+        { [styles.transparent]: isTransparent },
+        { [styles.grey]: isGrey },
+        { [styles.contrast]: hasContrast },
+        { [styles.disabled]: isDisabled })
+      }
       {...domProps}
     >
       {children}
