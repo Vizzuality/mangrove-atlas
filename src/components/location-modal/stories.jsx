70f923548cb2563c526711199026573a6e2de9d4
maria <maluenarod@gmail.com>
2019-07-18 16:42:48 +0200

    location-modal stories

--- src/components/location-modal/stories.jsx
+++ src/components/location-modal/stories.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withProvider } from 'utils/storybookProvider';
 import LocationSelector from './component';
 
 
 storiesOf('Location modal', module)
+  .addDecorator(withProvider)
   .add('open', () => (
     <LocationSelector isOpened />
-  ))
-  .add('close', () => (
-    <LocationSelector isClosed />
   ));
