2ee01714c1cc768b05915a9f4155746a24ae2c28
maria <maluenarod@gmail.com>
2019-07-18 16:45:31 +0200

    button group stories

--- src/components/buttonGroup/stories.jsx
+++ src/components/buttonGroup/stories.jsx
@@ -6,8 +6,8 @@ import Button from '../button/component';
 storiesOf('Button group', module)
   .add('buttons bar', () => (
     <ButtonGroup>
-      <Button hasBrackground>Opción 1</Button>
-      <Button hasBrackground>Opción 2</Button>
-      <Button hasBrackground>Opción 3</Button>
+      <Button>Opción 1</Button>
+      <Button>Opción 2</Button>
+      <Button>Opción 3</Button>
     </ButtonGroup>
   ));
