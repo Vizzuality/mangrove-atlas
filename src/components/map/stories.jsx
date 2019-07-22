9fb6e223501d70a83f31a6ba1c87d837a9ecc804
maria <maluenarod@gmail.com>
2019-07-22 11:54:11 +0200

    map temp container

--- src/components/map/stories.jsx
+++ src/components/map/stories.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
 import { withProvider } from 'utils/storybookProvider';
 import Map from './component';
 
 storiesOf('Map', module)
-  .addDecorator(withKnobs)
   .addDecorator(withProvider)
   .add('map', () => (
-    <div style={{ width: 500, height: 400 }}>
-    <Map viewport={{ width: 500, height: 400 }} />
+    <div style={{ width: '400px', height: '300px' }}>
+      <Map />
     </div>
   ));
