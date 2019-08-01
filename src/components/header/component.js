1f4931fe202396210333455d1753b37ef7d56c97
maria <maluenarod@gmail.com>
2019-08-01 16:33:40 +0200

    dashboard component for scroll

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -1,8 +1,11 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import OnScroll from 'react-on-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import background from './bg-shape.svg';
+import fixedBackground from './bg-fixed.svg';
 import styles from './style.module.scss';
 
 class Header extends PureComponent {
@@ -12,6 +15,10 @@ class Header extends PureComponent {
     }),
     openSearchPanel: PropTypes.func
   }
+
+  state = {
+    isFixed: false
+  };
 
   static defaultProps = {
     location: { name: 'Location name' },
@@ -24,8 +31,11 @@ class Header extends PureComponent {
     openSearchPanel();
   }
 
+  setSticky = isFixed => this.setState({ isFixed });
+
   render() {
     const { location } = this.props;
+    const { isFixed } = this.state;
 
     let stylesOverride = { fontSize: 60, lineHeight: 0.85 };
 
@@ -33,21 +43,47 @@ class Header extends PureComponent {
     if (location && location.name.length > 30) stylesOverride = { fontSize: 30, lineHeight: 1 };
 
     return (
-      <div className={styles.header}>
-        {location && (
-          <button type="button" className={styles.titleBtn} onClick={this.clickHandler}>
-            <h1
-              className={classnames(styles.title, 'notranslate')}
-              style={stylesOverride}
-            >
-              {location.name}
-            </h1>
-          </button>
-        )}
-        <button type="button" onClick={this.clickHandler} className={styles.searchButton}>
-          <FontAwesomeIcon icon={faSearch} size="lg" />
-        </button>
-      </div>
+      // <StickyBox offsetTop={20} offsetBottom={20}>
+      <OnScroll
+        triggers={[
+          { top: -40, callback: visible => this.setSticky(visible) },
+        ]}
+      >
+        <div className={styles.header}>
+          <img
+            className={classnames(styles.bg,
+              { [styles.visible]: !isFixed },
+              { [styles.invisible]: isFixed })}
+            src={background}
+            alt="Background"
+          />
+          <img
+            className={classnames(styles.bg,
+              { [styles.visible]: isFixed },
+              { [styles.invisible]: !isFixed })}
+            src={fixedBackground}
+            alt="Background"
+          />
+          <div className={classnames(styles.searchBar,
+            { [styles.fixed]: isFixed })}
+          >
+            {location && (
+              <button type="button" className={styles.titleBtn} onClick={this.clickHandler}>
+                <h1
+                  className={classnames(styles.title, 'notranslate')}
+                  style={stylesOverride}
+                >
+                  {location.name}
+                </h1>
+              </button>
+            )}
+            <button type="button" onClick={this.clickHandler} className={styles.searchButton}>
+              <FontAwesomeIcon icon={faSearch} size="lg" />
+            </button>
+          </div>
+        </div>
+      </OnScroll>
+      // </StickyBox>
     );
   }
 }
