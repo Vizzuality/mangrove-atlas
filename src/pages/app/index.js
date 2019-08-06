46365d29ea37f2312116872f900c4ed0651098b2
maria <maluenarod@gmail.com>
2019-08-06 13:32:55 +0200

    react responsive and main layout

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -3,18 +3,26 @@ import Widgets from 'components/widgets';
 import Map from 'components/map';
 import LocationModal from 'components/location-modal';
 import Sidebar from 'components/sidebar';
+import MediaQuery from 'react-responsive';
 import styles from './style.module.scss';
+
 
 const AppPage = () => (
   <div className={styles.app}>
-    <Sidebar>
-      <Widgets />
-    </Sidebar>
-    
-    <div className={styles.vis}>
-      <Map />
-    </div>
+    <MediaQuery maxWidth={384}>
+      <Sidebar>
+        <Widgets />
+      </Sidebar>
+    </MediaQuery>
+    <MediaQuery minWidth={384}>
+      <Sidebar>
+        <Widgets />
+      </Sidebar>
+      <div className={styles.vis}>
+        <Map />
+      </div>
 
+    </MediaQuery>
     {/* Modals */}
     <LocationModal />
   </div>
