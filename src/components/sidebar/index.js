1f69b9173385f952ebdf1984f8801e214b565a6e
maria <maluenarod@gmail.com>
2019-08-07 15:05:30 +0200

    action bar in header

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 import { setHeader } from 'modules/pages/actions';
+import { expandAll, collapseAll } from 'modules/widgets/actions';
 import Component from './component';
 
 const mapStateToProps = state => ({
-  header: state.page.header
+  header: state.page.header,
+  isCollapsed: state.widgets.isCollapsed
 });
 
 const mapDispatchToProps = {
-  setHeader
+  setHeader,
+  collapseAll,
+  expandAll
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Component);
