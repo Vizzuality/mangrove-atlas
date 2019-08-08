ab74a7e95be64646a3f932ef4e724e67ec1ecbad
David Inga <david.inga@vizzuality.com>
2019-08-08 15:15:36 +0200

    mangrove coverage migrated

--- src/components/widget/templates/mangrove-net-change/index.js
+++ src/components/widget/templates/mangrove-net-change/index.js
@@ -1,12 +1,3 @@
-import { connect } from 'react-redux';
-import { netChangeWidget } from 'modules/widgets/selectors';
-import { currentLocation } from 'modules/locations/selectors';
 import Component from './component';
 
-const mapStateToProps = state => ({
-  // data: netChangeWidget(state),
-  data: {},
-  location: currentLocation(state)
-});
-
-export default connect(mapStateToProps)(Component);
+export default Component;
