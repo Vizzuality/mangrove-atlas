67942f7e4bec2a59411459f532edde4a54108cf9
Migsar <migsar.navarro@vizzuality.com>
2019-08-13 17:37:12 +0100

    Feature/widget alerts (#44)
    * changed locations
    
    * locations integration from api
    
    * adde data for dashboards, widgets and layers
    
    * locations with geom
    
    * using data instead fetch
    
    * connected data
    
    * changes
    
    * coverage
    
    * mangrove data
    
    * connected widgets
    
    * widget api fix
    
    * mangrove coverage integrated
    
    * mangrove coverage migrated
    
    * fix(NA): Change Back to worldwide id from global to worldwide.
    
    * feat(NA): Basic layout for alerts widget.
    
    * refactor: Refactor WidgetList and Widget to be more clear.
    
    * fix(NA): Layer toggle was not working after API refactor.
    
    * refactor(NA): Start refactoring chart component.
    
    * fix(NA): Requested changes.

--- src/components/widget/templates/index.js
+++ src/components/widget/templates/index.js
@@ -1,11 +1,13 @@
+import HighlightedPlaces from './highlighted-places';
 import MangroveCoverage from './mangrove-coverage';
 import MangroveNetChange from './mangrove-net-change';
 import MangroveActivity from './mangrove-activity';
-import HighlightedPlaces from './highlighted-places';
+import MangroveAlerts from './mangrove-alerts';
 
 export default {
   mangrove_coverage: MangroveCoverage,
   mangrove_net_change: MangroveNetChange,
   mangrove_activity: MangroveActivity,
   highlighted_places: HighlightedPlaces,
+  mangrove_alerts: MangroveAlerts
 };
