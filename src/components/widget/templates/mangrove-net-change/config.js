33d8964bd77c8184f3a5fe80e3a7887750197bff
María Luena Rodríguez <maluenarod@gmail.com>
2019-08-14 12:01:22 +0200

    widget conected (#47)
    * widget conected
    
    * net change widget data
    
    * extra file removed

--- src/components/widget/templates/mangrove-net-change/config.js
+++ src/components/widget/templates/mangrove-net-change/config.js
@@ -2,130 +2,116 @@ import React from 'react';
 import { format } from 'd3-format';
 import WidgetLegend from 'components/widget/legend';
 import WidgetTooltip from 'components/widget/tooltip';
+import moment from 'moment';
+import orderBy from 'lodash/orderBy';
+
 
 const numberFormat = format(',.2f');
 
-export const CONFIG = {
-  parse: () => {
-    const fakeData = [
-      {
-        label: '1996',
-        year: 1996,
-        gain: 122,
-        netChange: 40,
-        loss: -120
-      },
-      {
-        label: '2002',
-        year: 2002,
-        gain: 155,
-        netChange: 30,
-        loss: -40
-      },
-      {
-        label: '2009',
-        year: 2009,
-        gain: 194,
-        netChange: 72,
-        loss: -182
-      },
-      {
-        label: '2016',
-        year: 2016,
-        gain: 135,
-        netChange: 110,
-        loss: -194
-      }
-    ];
-
-    return {
-      chartData: fakeData.map(l => (
-        {
-          x: l.label,
-          'Net change': l.netChange,
-          Gain: l.gain,
-          Loss: l.loss,
-          color: l.color,
-          name: l.label,
-          year: l.year
-        }
-      )),
-      chartConfig: {
-        stackOffset: 'sign',
-        margin: { top: 20, right: 0, left: 40, bottom: 0 },
-        referenceLines: [
-          { y: 0, label: null, stroke: 'rgba(0,0,0,0.85)' }
-        ],
-        xKey: 'year',
-        yKeys: {
-          lines: {
-            'Net change': {
-              stroke: 'rgba(0,0,0,0.7)'
-            }
-          },
-          bars: {
-            Gain: {
-              barSize: 10,
-              transform: `translate(${(4 + 10) / 2}, 0)`,
-              fill: '#077FAC',
-              radius: [10, 10, 0, 0]
-            },
-            Loss: {
-              barSize: 10,
-              transform: `translate(-${(4 + 10) / 2}, 0)`,
-              fill: '#EB6240',
-              radius: [10, 10, 0, 0]
-            }
-          }
-        },
-        xAxis: {
-          tick: { fontSize: 12, fill: '#AAA' }
-        },
-        yAxis: {
-          tick: { fontSize: 12, fill: '#AAA' },
-          tickFormatter: (v) => {
-            const result = v / 1000000;
-            return numberFormat(result);
-          },
-          tickMargin: 15,
-          domain: [-300, 300]
-        },
-        cartesianGrid: {
-          vertical: false,
-          strokeDasharray: '6 6'
-        },
-        legend: {
-          align: 'center',
-          verticalAlign: 'top',
-          layout: 'horizontal',
-          height: 50,
-          content: (properties) => {
-            const { payload } = properties;
-            const groups = { groups: payload };
-            return <WidgetLegend direction="vertical" groups={groups} />;
-          }
-        },
-        tooltip: {
-          cursor: false,
-          content: (
-            <WidgetTooltip
-              style={{
-                color: '#FFFFFF',
-                backgroundColor: '#383838'
-              }}
-              settings={[
-                { key: 'year' },
-                { key: 'Gain', format: value => `Gain: ${numberFormat(value / 1000000)} km2` },
-                { key: 'Loss', format: value => `Loss: ${numberFormat(value / 1000000)} km2` },
-                { key: 'Net change', format: value => `Net change: ${numberFormat(value / 1000000)} km2` }
-              ]}
-            />
-          )
-        }
-      }
-    };
-  }
+const widgetData = ({ list }) => {
+  const data = list.map(l => (
+    {
+      label: JSON.stringify(moment(l.date).year()),
+      year: moment(l.date).year(),
+      gain: l.gain_m2,
+      netChange: l.gain_m2 - l.loss_m2,
+      loss: -l.loss_m2
+    })).filter(l => l.netChange !== 0);
+  return orderBy(data, l => l.year);
 };
 
+const widgetMetadata = ({ list }) => ({
+  years: list.filter(l => (l.gain_m2 !== null && l.loss_m2 !== null)).map(l => (
+    moment(l.date).year()
+  )).sort((a, b) => a - b)
+});
+
+const CONFIG = {
+  parse: data => ({
+    chartData: widgetData(data).map(l => (
+      {
+        x: l.label,
+        'Net change': l.netChange,
+        Gain: l.gain,
+        Loss: l.loss,
+        name: l.label,
+        year: l.year
+      })),
+    metadata: widgetMetadata(data),
+    chartConfig: {
+      stackOffset: 'sign',
+      margin: { top: 20, right: 0, left: 40, bottom: 0 },
+      referenceLines: [
+        { y: 0, label: null, stroke: 'rgba(0,0,0,0.85)' }
+      ],
+      xKey: 'year',
+      yKeys: {
+        lines: {
+          'Net change': {
+            stroke: 'rgba(0,0,0,0.7)'
+          }
+        },
+        bars: {
+          Gain: {
+            barSize: 10,
+            transform: `translate(${(4 + 10) / 2}, 0)`,
+            fill: '#077FAC',
+            radius: [10, 10, 0, 0]
+          },
+          Loss: {
+            barSize: 10,
+            transform: `translate(-${(4 + 10) / 2}, 0)`,
+            fill: '#EB6240',
+            radius: [10, 10, 0, 0]
+          }
+        }
+      },
+      xAxis: {
+        tick: { fontSize: 12, fill: '#AAA' }
+      },
+      yAxis: {
+        tick: { fontSize: 12, fill: '#AAA' },
+        tickFormatter: (v) => {
+          const result = v / 1000000;
+          return numberFormat(result);
+        },
+        tickMargin: 15,
+        domain: [-300, 300]
+      },
+      cartesianGrid: {
+        vertical: false,
+        strokeDasharray: '6 6'
+      },
+      legend: {
+        align: 'center',
+        verticalAlign: 'top',
+        layout: 'horizontal',
+        height: 50,
+        content: (properties) => {
+          const { payload } = properties;
+          const groups = { groups: payload };
+          return <WidgetLegend direction="vertical" groups={groups} />;
+        }
+      },
+      tooltip: {
+        cursor: false,
+        content: (
+          <WidgetTooltip
+            style={{
+              color: '#FFFFFF',
+              backgroundColor: '#383838'
+            }}
+            settings={[
+              { key: 'year' },
+              { key: 'Gain', format: value => `Gain: ${numberFormat(value / 1000000)} km2` },
+              { key: 'Loss', format: value => `Loss: ${numberFormat(value / 1000000)} km2` },
+              { key: 'Net change', format: value => `Net change: ${numberFormat(value / 1000000)} km2` }
+            ]}
+          />
+        )
+      }
+    }
+  })
+};
 
 export default CONFIG;
