384df6ce3a9d89525b30e8e4fe07f103c75aee8d
María Luena Rodríguez <maluenarod@gmail.com>
2019-08-14 12:01:22 +0200

    widget conected (#47)
    * widget conected
    
    * net change widget data
    
    * extra file removed

--- src/components/widget/templates/mangrove-net-change/component.js
+++ src/components/widget/templates/mangrove-net-change/component.js
@@ -5,7 +5,6 @@ import Chart from 'components/chart';
 import Select from 'components/select';
 import DownloadLink from 'components/link';
 import sumBy from 'lodash/sumBy';
-
 import styles from 'components/widget/style.module.scss';
 
 const numberFormat = format(',.2f');
@@ -13,14 +12,13 @@ const numberFormat = format(',.2f');
 class MangroveNetChange extends PureComponent {
   static propTypes = {
     data: PropTypes.shape({}),
-    chartConfig: PropTypes.shape({}).isRequired,
-    location: PropTypes.shape({}),
+    currentLocation: PropTypes.shape({}),
     slug: PropTypes.string
   }
 
   static defaultProps = {
     data: null,
-    location: null,
+    currentLocation: null,
     slug: null
   }
 
@@ -30,11 +28,11 @@ class MangroveNetChange extends PureComponent {
   }
 
   getData() {
-    const { data: { widgetData } } = this.props;
+    const { data: { chartData } } = this.props;
     const { startYear, endYear } = this.state;
     const y0 = parseInt(startYear, 0);
     const y1 = parseInt(endYear, 0);
-    const result = widgetData.filter((d) => {
+    const result = chartData.filter((d) => {
       const y = parseInt(d.year, 0);
       return y >= y0 && y <= y1;
     });
@@ -46,22 +44,21 @@ class MangroveNetChange extends PureComponent {
   changeEndYear = endYear => this.setState({ endYear })
 
   render() {
-    const { data: { metadata }, chartConfig, location, slug } = this.props;
+    const { data: { metadata, chartConfig }, currentLocation, slug } = this.props;
     const { startYear, endYear } = this.state;
     const optionsYears = metadata.years.map(year => ({
-      label: year,
-      value: year
+      label: year.toString(),
+      value: year.toString()
     }));
     const widgetData = this.getData();
     const totalLoss = widgetData && widgetData.length
       ? Math.abs(sumBy(widgetData, 'Loss'))
       : 0;
-
     return (
       <Fragment>
         <div className={styles.widget_template}>
           <div className={styles.sentence}>
-            <span>Mangroves in</span> <strong>{location.type === 'worldwide' ? 'the world' : <span className="notranslate">{location.name}</span>}</strong>
+            <span>Mangroves in</span> <strong>{currentLocation.type === 'worldwide' ? 'the world' : <span className="notranslate">{currentLocation.name}</span>}</strong>
             {' '}<span>have</span> <strong>decreased</strong> by <strong className="notranslate">{numberFormat(totalLoss / 100000)} km<sup>2</sup></strong>{' '}<br />
             <span>between</span>
             {' '}
@@ -70,7 +67,7 @@ class MangroveNetChange extends PureComponent {
               prefix="start-year"
               value={startYear}
               options={optionsYears}
-              isOptionDisabled={option => parseInt(option.value) > parseInt(endYear) || option.value === startYear}
+              isOptionDisabled={option => parseInt(option.value, 10) > parseInt(endYear, 10) || option.value === startYear}
               onChange={this.changeStartYear}
             />
             {' '}<span>and</span>{' '}
@@ -79,7 +76,7 @@ class MangroveNetChange extends PureComponent {
               prefix="end-year"
               value={endYear}
               options={optionsYears}
-              isOptionDisabled={option => parseInt(option.value) < parseInt(startYear) || option.value === endYear}
+              isOptionDisabled={option => parseInt(option.value, 10) < parseInt(startYear, 10) || option.value === endYear}
               onChange={this.changeEndYear}
             />
             {'.'}
