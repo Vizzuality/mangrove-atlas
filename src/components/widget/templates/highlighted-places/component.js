8b0c23051c4b30adc7baa2afdac19875f506c4d0
María Luena Rodríguez <maluenarod@gmail.com>
2019-09-05 04:43:28 +0200

    Feature/highlighted design (#66)
    * images added to card
    
    * link to card
    
    * link hover removed
    
    * extra card for worldwide
    
    * card mobile styles
    
    * hotspots card styles

--- src/components/widget/templates/highlighted-places/component.js
+++ src/components/widget/templates/highlighted-places/component.js
@@ -1,27 +1,59 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Link from 'redux-first-router-link';
 import styles from './style.module.scss';
+import Rufiji from './images/rufiji.jpg';
+import Saloum from './images/saloum.jpg';
+import Worldwide from './images/worldwide.jpg';
+
 
 const HighlightedPlaces = ({ data, currentLocation }) => (
-  <Fragment>
-    <div className={styles.hotspotsList}>
-      {data.map(d => (
+  <div className={styles.hotspotsList}>
+    {
+      data.map(d => (
+        <Link to={{ type: 'PAGE/AOI', payload: { id: d.id } }}>
+          {d.id !== currentLocation.id && (
+            <div
+              key={d.id}
+              style={{
+                backgroundImage: `linear-gradient(0deg, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 64.18%, rgba(0,0,0,0) 100%), url(${d.name === 'Rufiji' ? Rufiji : Saloum})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover'
+              }}
+              className={classnames(styles.card, { [styles.active]: d.id === currentLocation.id })}
+            >
+              <span className={styles.cardInfo}>
+                <h3 className="notranslate">{d.name}</h3>
+                <p><span className="notranslate">{d.coast_length_m}</span> <span className="notranslate">{d.unit}</span></p>
+              </span>
+              {/* <span className={styles.cardInfo}>View place</span> */}
+            </div>
+          )}
+        </Link>
+      ))
+    }
+    {currentLocation.location_type !== 'worldwide' && (
+      <Link to={{ type: 'PAGE/APP' }}>
         <div
-          key={d.id}
-          className={classnames(styles.card, { [styles.active]: d.id === currentLocation.id })}
+          key={currentLocation.id}
+          style={{
+            backgroundImage: `url(${Worldwide})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover'
+          }}
+          className={styles.card}
         >
           <span className={styles.cardInfo}>
-            <h3 className="notranslate">{d.name}</h3>
-            <p><span className="notranslate">{d.coast_length_m}</span> <span className="notranslate">{d.unit}</span> <span>coastline</span></p>
+            <h3 className="notranslate">Worlwide</h3>
           </span>
-          <Link to={{ type: 'PAGE/AOI', payload: { id: (d.id === currentLocation.id) ? 'worldwide' : d.id } }}><span className={styles.link}>{(d.id === currentLocation.id) ? 'Back to Worldwide' : 'View place' }</span></Link>
+          {/* <span className={styles.cardInfo}>Back to Worldwide</span> */}
         </div>
-      ))}
-    </div>
-  </Fragment>
+      </Link>
+    )}
+  </div>
 );
+
 
 HighlightedPlaces.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({})),
