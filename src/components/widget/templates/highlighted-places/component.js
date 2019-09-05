ff40b90dbd0550060e0ed9a0423b46acc8771deb
María Luena Rodríguez <maluenarod@gmail.com>
2019-09-05 11:49:50 +0200

    hotspot images (#85)

--- src/components/widget/templates/highlighted-places/component.js
+++ src/components/widget/templates/highlighted-places/component.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Link from 'redux-first-router-link';
 import styles from './style.module.scss';
-import Rufiji from './images/Rufiji.jpg';
-import Saloum from './images/Saloum.jpg';
-import Worldwide from './images/Worldwide.jpg';
+import Rufiji from './images/rufiji.jpg';
+import Saloum from './images/saloum.jpg';
+import Worldwide from './images/worldwide.jpg';
 
 
 const HighlightedPlaces = ({ data, currentLocation }) => (
