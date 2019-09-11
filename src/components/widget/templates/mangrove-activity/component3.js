1a852f8c0d7c9f5d2aad94365d03c8152199a539
Migsar Navarro <migsar.navarro@vizzuality.com>
2019-09-11 06:19:37 +0100

    wip: integration activity widget real data.

--- src/components/widget/templates/mangrove-activity/component3.js
+++ src/components/widget/templates/mangrove-activity/component3.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import orderBy from 'lodash/orderBy';
+import flatten from 'lodash/flatten';
 
 import Spinner from 'components/spinner';
 import Chart from 'components/chart';
 import Select from 'components/select';
+
+import { scaleLinear } from 'd3-scale';
 
 import styles from 'components/widget/style.module.scss';
 
@@ -28,24 +31,110 @@ class MangroveActivity extends React.PureComponent {
     }
   }
 
+  changeUnit = (unit) => {
+    this.setState({ unit });
+  }
+
   changeFilter = (filter) => {
     this.setState({ filter });
   }
 
-  getRanking = (fakeData, filter) => orderBy(
-    fakeData[filter], d => Math.abs(d[filter])
+  getRanking = (chartData, filter) => orderBy(
+    chartData[filter], d => Math.abs(d[filter])
   ).reverse().map((f, index) => ({ ...f, x: index }));
 
+  getConfig = () => {
+    const { data: { chartConfig, chartData } } = this.props;
+    const { filter } = this.state;
+
+    const dataRanked = this.getRanking(chartData, filter);
+
+    const max = Math.max(...flatten(chartData[filter]
+      .map(d => [Math.abs(d.gain), Math.abs(d.loss)])));
+    const domainX = [-max + (-max * 0.05), max + (max * 0.05)];
+
+    return {
+      ...chartConfig,
+      xAxis: {
+        type: 'number',
+        domain: domainX,
+        interval: 0
+      },
+      yKeys: {
+        lines: {
+          net: {
+            barSize: 10,
+            fill: 'rgba(0,0,0,0.7)',
+            radius: 4,
+            legend: 'Net result',
+          }
+        },
+        bars: {
+          gain: {
+            barSize: 10,
+            fill: '#077FAC',
+            radius: [0, 10, 10, 0],
+            legend: 'Gain',
+            stackId: 'stacked',
+            label: {
+              content: (prs) => {
+                const w = this.chart.offsetWidth;
+
+                const { index, y } = prs;
+                const { name } = dataRanked[index];
+
+                return (
+                  <g className={styles.activity_widget}>
+                    <text className={styles.label} x={w / 2} y={y - 15} textAnchor="middle" fill="#000">
+                      {name}
+                    </text>
+                  </g>
+                );
+              }
+            }
+          },
+          loss: {
+            barSize: 10,
+            fill: '#EB6240',
+            radius: [0, 10, 10, 0],
+            stackId: 'stacked',
+            legend: 'Loss',
+            label: {
+              content: (prs) => {
+                const w = this.chart.offsetWidth;
+
+                const { index, y } = prs;
+                const { loss, gain } = dataRanked[index];
+                const net = loss + gain;
+
+                const scale = scaleLinear()
+                  .domain(domainX)
+                  .range([0, w]);
+
+                const x = scale((net));
+
+                return (
+                  <g>
+                    <rect x={x} y={y - 5} width={2} height={20} fill="#000" />
+                  </g>
+                );
+              }
+            }
+          }
+        },
+      }
+    };
+  }
 
   render() {
-    const { data: { chartData, metadata, chartConfig } } = this.props;
+    const { data: { chartData } } = this.props;
     const { yearStart, yearEnd, unit, filter } = this.state;
 
     // XXX: these options should come from an api ?
     const optionsFilter = [
-      { value: 'gain', label: 'gain' },
-      { value: 'loss', label: 'loss' },
-      { value: 'net', label: 'net' }
+      { value: 'gain', label: 'Gain' },
+      { value: 'loss', label: 'Loss' },
+      { value: 'net', label: 'Net' }
     ];
 
     const optionsYearStart = [
@@ -58,8 +147,9 @@ class MangroveActivity extends React.PureComponent {
       { value: '2019', label: '2019' }
     ];
 
-    // Selectors
+    const change = 5;
 
+    // Selectors
     const filterSelector = (
       <Select
         value={filter}
@@ -89,16 +179,16 @@ class MangroveActivity extends React.PureComponent {
         <div className={styles.widget_template}>
           <div className={styles.sentence}>
             {/* eslint-disable-next-line */}
-            Regions of interest within location showed relative {filterSelector} of <strong>{metadata}{unit}</strong> between {startYearSelector} to {endYearSelector}.
+            Regions of interest within location showed relative {filterSelector} of <strong>{change}{unit}</strong> between {startYearSelector} to {endYearSelector}.
           </div>
         </div>
 
         {/* Chart */}
-        {!chartData.length && <Spinner />}
+        {!chartData && <Spinner />}
         <Chart
           onReady={(r) => { this.chart = r; }}
-          data={chartData}
-          config={chartConfig}
+          data={this.getRanking(chartData, filter)}
+          config={this.getConfig()}
         />
       </Fragment>
     );
--- src/components/widget/templates/mangrove-activity/config.js
+++ src/components/widget/templates/mangrove-activity/config.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 import orderBy from 'lodash/orderBy';
-import flatten from 'lodash/flatten';
-
-import { scaleLinear } from 'd3-scale';
-
-import styles from 'components/widget/style.module.scss';
 
 // Utils
 import { format } from 'd3-format';
@@ -16,42 +11,113 @@ import WidgetLegend from 'components/widget/legend';
 
 const numberFormat = format(',.2f');
 
-const widgetData = (activityData) => {
-  if (!activityData) return null;
-  console.log(activityData, 'activityData')
-  const formattedData = activityData.map(p => (p.mangrove_datum.map(l => (
+const fakeData = {
+  gain: [
     {
-      label: p.name,
-      year: moment(l.date).year(),
-      gain: l.gain_m2,
-      net: l.net_change_m2,
-      loss: l.loss_m2
-    })).filter(l => l.net !== 0)
-  ));
-  return orderBy(formattedData, l => l.year);
+      name: 'place 1',
+      label: '1996',
+      gain: 13,
+      loss: -120
+    },
+    {
+      name: 'place 2',
+      label: '2002',
+      gain: 15,
+      loss: -40
+    },
+    {
+      name: 'place 3',
+      label: '2009',
+      gain: 19,
+      loss: -18
+    },
+    {
+      x: 3,
+      name: 'place 4',
+      label: '2016',
+      gain: 20,
+      net: 110,
+      loss: -194
+    }
+  ],
+  loss: [
+    {
+      name: 'place',
+      label: '1996',
+      gain: 122,
+      net: 40,
+      loss: -12
+    },
+    {
+      name: 'place',
+      label: '2002',
+      gain: 155,
+      net: 30,
+      loss: -4
+    },
+    {
+      name: 'place',
+      label: '2009',
+      gain: 194,
+      net: 72,
+      loss: -18
+    },
+    {
+      name: 'place',
+      label: '2016',
+      gain: 135,
+      net: 110,
+      loss: -19
+    }
+  ],
+  net: [
+    {
+      name: 'place',
+      label: '1996',
+      gain: 122,
+      net: 40,
+      loss: -120
+    },
+    {
+      name: 'place',
+      label: '2002',
+      gain: 155,
+      net: 30,
+      loss: -40
+    },
+    {
+      name: 'place',
+      label: '2015',
+      gain: 194,
+      net: 72,
+      loss: -182
+    },
+    {
+      name: 'place',
+      label: '2016',
+      gain: 135,
+      net: 110,
+      loss: -194
+    }
+  ]
 };
 
-const widgetMetadata = (activityData) => {
-  const dates = activityData.map(p => (p.mangrove_datum.map(l => (
-    moment(l.date).year()
-  )).filter(l => l.netChange !== 0)
-  ));
-  return dates;
-};
-
-const getDomain = (activityData) => {
-  const data = widgetData(activityData);
-  if (!data) return [1996, 2005, 2007];
-  const max = Math.max(...flatten(data
-    .map(d => [Math.abs(d.gain), Math.abs(d.loss)])));
-  return [-max + (-max * 0.05), max + (max * 0.05)];
+const widgetData = (data) => {
+  return {
+    gain: data.map(location => ({
+      name: location.name,
+      gain: location.mangrove_datum[0].gain_m2,
+      loss: - location.mangrove_datum[0].loss_m2,
+      net: location.mangrove_datum[0].gain_m2 - location.mangrove_datum[0].loss_m2
+    })),
+    loss: fakeData.loss,
+    net: fakeData.net
+  };
 };
 
 export const CONFIG = {
   parse: data => ({
     chartData: widgetData(data),
-    metadata: [ 1996, 2005, 2007],
-  //  metadata: widgetMetadata(data),
     chartConfig: {
       layout: 'vertical',
       referenceLines: [
@@ -73,75 +139,12 @@ export const CONFIG = {
           }));
           return <WidgetLegend direction="vertical" groups={{ labels }} />;
         }
+
+
       },
       xAxis: {
         type: 'number',
-        domain: getDomain(),
-        interval: 0,
         tick: true,
-      },
-      yKeys: {
-        lines: {
-          net: {
-            barSize: 10,
-            fill: 'rgba(0,0,0,0.7)',
-            radius: 4,
-            legend: 'Net result',
-          }
-        },
-        bars: {
-          gain: {
-            barSize: 10,
-            fill: '#077FAC',
-            radius: [0, 10, 10, 0],
-            legend: 'Gain',
-            stackId: 'stacked',
-            label: {
-              content: (prs) => {
-                const w = 12;
-
-                const { index, y } = prs;
-                const { name } = data[index];
-
-                return (
-                  <g className={styles.activity_widget}>
-                    <text className={styles.label} x={w / 2} y={y - 15} textAnchor="middle" fill="#000">
-                      {name}
-                    </text>
-                  </g>
-                );
-              }
-            }
-          },
-          loss: {
-            barSize: 10,
-            fill: '#EB6240',
-            radius: [0, 10, 10, 0],
-            stackId: 'stacked',
-            legend: 'Loss',
-            label: {
-              content: (prs) => {
-                const w = 12  ;
-
-                const { index, y } = prs;
-                const { loss, gain } = data[index];
-                const net = loss + gain;
-
-                const scale = scaleLinear()
-                  .domain(getDomain(data))
-                  .range([0, w]);
-
-                const x = scale((net));
-
-                return (
-                  <g>
-                    <rect x={x} y={y - 5} width={2} height={20} fill="#000" />
-                  </g>
-                );
-              }
-            }
-          }
-        },
       },
       cartesianGrid: {
         vertical: true,
