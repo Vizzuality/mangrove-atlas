21a5ed335e6069e9c05db495123f8f9f88532371
María Luena <maria.luena@vizzuality.com>
2019-09-18 16:10:52 +0200

    locations modal adapted to refactor

--- src/components/widget/component.js
+++ src/components/widget/component.js
@@ -13,17 +13,18 @@ import styles from './style.module.scss';
 
 function Widget({
   children,
+  data,
+  name,
   slug,
-  toggleCollapse,
-  toggleActive,
-  layersIds,
+  filename,
   layerId,
+  layersIds,
   isActive,
+  isLocationsModal,
   isCollapsed,
   isLoading,
-  name,
-  data,
-  filename
+  toggleActive,
+  toggleCollapse
 }) {
   const collapseToggleHandler = () => {
     toggleCollapse({ id: slug });
@@ -43,36 +44,42 @@ function Widget({
     <div
       className={
         classnames(styles.widget, {
-          [styles.collapsed]: isCollapsed,
-          [styles.layerActive]: isActive
+          [styles._modal]: isLocationsModal,
+          [styles._collapsed]: isCollapsed,
+          [styles._layerActive]: isActive
         })
       }
     >
-      <div className={styles.wrapper}>
-        <div className={styles.header}>
-          <button
-            type="button"
-            className={styles.title}
-            onClick={collapseToggleHandler}
-          >
-            <MediaQuery minWidth={breakpoints.md}>
-              {isCollapsed
-                ? <FontAwesomeIcon icon={faChevronDown} />
-                : <FontAwesomeIcon icon={faChevronUp} />}
-            </MediaQuery>
-            {name}
-          </button>
-          {haveLayers && (
-            <Button
-              hasBackground={isActive}
-              hasContrast={!isActive}
-              isActive={isActive}
-              onClick={activeToggleHandler}
+      <div className={classnames(styles.wrapper, {
+        [styles._modal]: isLocationsModal
+      })}>
+        {!isLocationsModal &&
+          <div className={styles.header}>
+            <button
+              type="button"
+              className={styles.title}
+              onClick={collapseToggleHandler}
             >
-              {isActive ? 'Hide layer' : 'Show layer'}
-            </Button>
-          )}
-        </div>
+              <MediaQuery minWidth={breakpoints.md}>
+                {isCollapsed
+                  ? <FontAwesomeIcon icon={faChevronDown} />
+                  : <FontAwesomeIcon icon={faChevronUp} />}
+              </MediaQuery>
+              {name}
+            </button>
+            {haveLayers && (
+              <Button
+                hasBackground={isActive}
+                hasContrast={!isActive}
+                isActive={isActive}
+                onClick={activeToggleHandler}
+              >
+                {isActive ? 'Hide layer' : 'Show layer'}
+              </Button>
+            )}
+          </div>
+        }
+
         {isLoading
           ? <Spinner isLoading />
           : (
@@ -82,31 +89,38 @@ function Widget({
           )
         }
       </div>
-      <WidgetInfo data={data} filename={filename} />
+
+      {!isLocationsModal &&
+        <WidgetInfo data={data} filename={filename} />
+      }
     </div>
   );
 }
 
 Widget.propTypes = {
+  data: PropTypes.shape({}).isRequired,
   name: PropTypes.string.isRequired,
-  slug: PropTypes.string.isRequired,
+  slug: PropTypes.string,
+  filename: PropTypes.string,
   layerId: PropTypes.string,
   layersIds: PropTypes.arrayOf(PropTypes.string),
-  location: PropTypes.shape({}),
   isActive: PropTypes.bool,
+  isLocationsModal: PropTypes.bool,
   isCollapsed: PropTypes.bool,
   isLoading: PropTypes.bool,
   toggleActive: PropTypes.func,
-  toggleCollapse: PropTypes.func
+  toggleCollapse: PropTypes.func,
 };
 
 Widget.defaultProps = {
-  isActive: false,
-  isCollapsed: false,
-  isLoading: false,
+  slug: null,
+  filename: null,
   layerId: null,
   layersIds: null,
-  location: null,
+  isActive: false,
+  isLocationsModal: false,
+  isCollapsed: false,
+  isLoading: false,
   toggleActive: () => { },
   toggleCollapse: () => { }
 };
