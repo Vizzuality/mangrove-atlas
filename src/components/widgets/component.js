3fb19b3070de2f8b34a5048ac6108d7f2094b764
María Luena <maria.luena@vizzuality.com>
2019-09-24 17:50:17 +0200

    bug in header

--- src/components/widgets/component.js
+++ src/components/widgets/component.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import OnScroll from 'react-on-scroll';
+import classnames from 'classnames';
 
 import styles from './style.module.scss';
 
-const WidgetList = ({ widgets, templates, ...parentProps }) => (
-  <div className={styles.widgets}>
-    {widgets.map((widget) => {
-      const Widget = templates.get(widget.slug).component;
+const WidgetList = ({ widgets, templates, ...parentProps }) => {
+  const [securityMargin, setMargin] = useState(false);
 
-      return (
-        <Widget
-          key={widget.slug}
-          {...widget}
-          {...parentProps}
-        />
-      );
-    })}
-  </div>
-);
+  return (
+    <OnScroll
+      triggers={[{ top: -65, callback: _securityMargin => setMargin(!_securityMargin) }]}
+    >
+      <div className={classnames(styles.widgets, {
+        [styles.securityMargin]: securityMargin
+      })}>
+        {widgets.map((widget) => {
+          const Widget = templates.get(widget.slug).component;
+
+          return (
+            <Widget
+              key={widget.slug}
+              {...widget}
+              {...parentProps}
+            />
+          );
+        })}
+      </div>
+    </OnScroll>)
+};
 
 WidgetList.propTypes = {
   widgets: PropTypes.arrayOf(
