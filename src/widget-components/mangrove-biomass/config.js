f2af6209c5015694afae09b84c76fb60b88aca7d
María Luena Rodríguez <maluenarod@gmail.com>
2019-10-03 09:42:34 +0200

    real data, biomass/height chages (#133)

--- src/widget-components/mangrove-biomass/config.js
+++ src/widget-components/mangrove-biomass/config.js
@@ -1,164 +1,219 @@
 
 import React from 'react';
 import groupBy from 'lodash/groupBy';
-import { format } from 'd3-format';
-import moment from 'moment';
+
+// Components
 import WidgetLegend from 'components/widget-legend';
 import WidgetTooltip from 'components/widget-tooltip';
 
+// Utils
+import { format } from 'd3-format';
+import moment from 'moment';
+import looseJsonParse from 'utils/loose-json-parse';
+import sortBy from 'lodash/sortBy';
+
+
+
 const numberFormat = format(',.3r');
 
-const sentenceData = data => {
-
-  const dataFiltered = data.filter(d => d.agb_mgha_1 !== null);
-
-  return {
-    data: dataFiltered.map(d => d.agb_mgha_1).reduce((previous, current) => current + previous),
-    year: dataFiltered.map(d => moment(d.date).year())
+const chunk = (array, size) => {
+  const chunked_arr = [];
+  for (let i = 0; i < array.length; i++) {
+    const last = chunked_arr[chunked_arr.length - 1];
+    if (!last || last.length === size) {
+      chunked_arr.push([array[i]]);
+    } else {
+      last.push(array[i]);
+    }
   }
+  return chunked_arr;
 }
 
+let maxValue = 0;
+
+const getBars = (barValues) => {
+  if (!barValues) return null;
+  const barsData = (Object.values(looseJsonParse(barValues)));
+  const chunkedData = chunk(barsData, 5)
+  const formattedData = chunkedData.map(
+    r => numberFormat((r.reduce((previous, current) => current + previous) / 1000))
+  );
+  maxValue = Math.max(...formattedData)
+  return formattedData;
+}
+
+
+const histogramData = (data) => {
+  if (!data) {
+    return null;
+  }
+
+  const histogram = data.map(d => (
+    {
+      year: moment(d.date).year(),
+      '0 50': getBars(d.agb_hist_mgha_1)[0],
+      '50 100': getBars(d.agb_hist_mgha_1)[1],
+      '100 150': getBars(d.agb_hist_mgha_1)[2],
+      '150 200': getBars(d.agb_hist_mgha_1)[3],
+      '200 250': getBars(d.agb_hist_mgha_1)[4],
+    }
+  ));
+  return histogram;
+}
+
+const filterData = data => data.filter(d => d.agb_mgha_1 !== null && d.agb_hist_mgha_1 !== null);
+
+const sentenceData = data => ({
+  height: data.map(d => d.agb_mgha_1).reduce((previous, current) => current + previous, 0),
+  year: data.map(d => moment(d.date).year())
+});
+
+const metaData = (data) => sortBy(data.map(d => moment(d.date).year()));
+
 export const CONFIG = {
-  parse: (data) => ({
-    biomassData: sentenceData(data),
-    chartData: [
-      { year: 1996, '0 50': 18, '50 100': 50, '100 150': 70, '150 200': 90, '200 250': 98 },
-      { year: 2007, '0 50': 30, '50 100': 40, '100 150': 60, '150 200': 70, '200 250': 75 },
-      { year: 2008, '0 50': 10, '50 100': 40, '100 150': 65, '150 200': 80, '200 250': 85 },
-      { year: 2009, '0 50': 15, '50 100': 42, '100 150': 67, '150 200': 82, '200 250': 90 },
-      { year: 2010, '0 50': 20, '50 100': 50, '100 150': 70, '150 200': 98, '200 250': 102 },
-    ],
-    metadata: [1996, 2007, 2008, 2009, 2010],
-    chartConfig: {
-      height: 300,
-      cartesianGrid: {
-        vertical: false,
-        horizontal: true,
-        strokeDasharray: '5 20'
-      },
-      margin: { top: 0, right: 50, left: 0, bottom: 0 },
-      xKey: 'year',
-      yKeys: {
-        bars:
-        {
-          '0 50':
-          {
-            stackId: 'bar',
-            fill: '#EAF19D',
-            stroke: '#EAF19D',
-            isAnimationActive: false
+  parse: (data) => {
+    {
+      const dataFiltered = filterData(data);
+      const metadata = metaData(dataFiltered)
+
+      return {
+        biomassData: sentenceData(dataFiltered),
+        metadata,
+        chartData: histogramData(dataFiltered),
+        chartConfig: {
+          height: 300,
+          cartesianGrid: {
+            vertical: false,
+            horizontal: true,
+            strokeDasharray: '5 20'
           },
-          '50 100':
-          {
-            stackId: 'bar',
-            fill: '#B8E98E',
-            stroke: '#B8E98E',
-            isAnimationActive: false
+          margin: { top: 0, right: 50, left: 0, bottom: 0 },
+          xKey: 'year',
+          yKeys: {
+            bars:
+            {
+              '0 50':
+              {
+                stackId: 'bar',
+                fill: '#EAF19D',
+                stroke: '#EAF19D',
+                isAnimationActive: false
+              },
+              '50 100':
+              {
+                stackId: 'bar',
+                fill: '#B8E98E',
+                stroke: '#B8E98E',
+                isAnimationActive: false
+              },
+              '100 150':
+              {
+                stackId: 'bar',
+                fill: '#1B97C1',
+                stroke: '#1B97C1',
+                isAnimationActive: false
+              },
+              '150 200':
+              {
+                stackId: 'bar',
+                fill: '#1C52A3',
+                stroke: '#1C52A3',
+                isAnimationActive: false
+              },
+              '200 250':
+              {
+                stackId: 'bar',
+                fill: '#13267F',
+                stroke: '#13267F',
+                isAnimationActive: false
+              }
+            }
           },
-          '100 150':
-          {
-            stackId: 'bar',
-            fill: '#1B97C1',
-            stroke: '#1B97C1',
-            isAnimationActive: false
+          referenceLines: [{
+            y: 0,
+            stroke: 'black',
+            strokeDasharray: 'solid',
+            fill: 'black',
+            opacity: '1',
+            label: null
+          }],
+          xAxis: {
+            tick: {
+              x: {
+                x: 0,
+                stroke: 'solid'
+              },
+              fontSize: 12,
+              lineHeight: 20,
+              fill: 'rgba(0,0,0,0.2)',
+              stroke: 'rgba(0,0,0,0.2)',
+              textShadow: '0 2px 4px 0 rgba(0,0,0,0.5)'
+            },
+            ticks: metadata,
+            domain: [metadata[0], metadata[metadata.length - 1]],
+            interval: 0
           },
-          '150 200':
-          {
-            stackId: 'bar',
-            fill: '#1C52A3',
-            stroke: '#1C52A3',
-            isAnimationActive: false
+          yAxis: {
+            tick: {
+              fontSize: 12, fill: 'rgba(0,0,0,0.54)'
+            },
+            domain: [0, maxValue + 50],
+            interval: 0,
+            orientation: 'right',
+            label: {
+              content: () => (
+                <g>
+                  <text
+                    x={415}
+                    y={25}
+                    style={
+                      { position: 'absolute' }
+                    }
+                    fontSize={11}
+                    fill="rgba(0,0,0,0.54)"
+                  >
+                    g ha<tspan baselineShift="super">-1</tspan>
+                  </text>
+                </g>
+              )
+            },
+            type: 'number'
           },
-          '200 250':
-          {
-            stackId: 'bar',
-            fill: '#13267F',
-            stroke: '#13267F',
-            isAnimationActive: false
+          legend: {
+            position: 'relative',
+            verticalAlign: 'top',
+            top: 10,
+            content: (properties) => {
+              const { payload } = properties;
+              const groups = groupBy(payload, p => p.payload);
+              return <WidgetLegend type="height" groups={groups} />;
+            }
+          },
+          tooltip: {
+            cursor: false,
+            content: (
+              <WidgetTooltip
+                type='column'
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-around',
+                  flexDirection: 'column'
+                }}
+                settings={[
+                  { label: '0 50:', color: '#EAF19D', key: '0 50', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked' },
+                  { label: '50 100:', color: '#B8E98E', key: '50 100', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked' },
+                  { label: '100 150:', color: '#1B97C1', key: '100 150', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked' },
+                  { label: '150 200:', color: '#1C52A3', key: '150 200', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked' },
+                  { label: '200 250:', color: '#13267F', key: '200 250', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked' },
+                ]}
+                label={{ key: 'name' }}
+              />
+            )
           }
-        }
-      },
-      referenceLines: [{
-        y: 0,
-        stroke: 'black',
-        strokeDasharray: 'solid',
-        fill: 'black',
-        opacity: '1',
-        label: null
-      }],
-      xAxis: {
-        tick: {
-          x: {
-            x: 0,
-            color: 'red',
-            stroke: 'solid'
-          },
-          fontSize: 12,
-          lineHeight: 20,
-          fill: 'rgba(0,0,0,0.2)',
-          stroke: 'rgba(0,0,0,0.2)',
-          textShadow: '0 2px 4px 0 rgba(0,0,0,0.5)'
         },
-        ticks: ['1996', '2007', '2008', '2009', '2010'],
-        domain: [1996, 2010],
-        interval: 0
-      },
-      yAxis: {
-        tick: {
-          fontSize: 12, fill: 'rgba(0,0,0,0.54)'
-        },
-        ticks: [0, 100, 200, 300],
-        interval: 0,
-        orientation: 'right',
-        label: {
-          content: () => (
-            <g>
-              <text
-                x={367}
-                y={45}
-                fontSize={11}
-                fill="rgba(0,0,0,0.54)"
-              >
-                mg ha<tspan baselineShift = "super">-1</tspan>
-              </text>
-            </g>
-          )
-        },
-        type: 'number'
-      },
-      legend: {
-        position: 'relative',
-        verticalAlign: 'top',
-        top: 10,
-        content: (properties) => {
-          const { payload } = properties;
-          const groups = groupBy(payload, p => p.payload);
-          return <WidgetLegend type="height" groups={groups} />;
-        }
-      },
-      tooltip: {
-        cursor: false,
-        content: (
-          <WidgetTooltip
-            type='column'
-            style={{
-              display: 'flex',
-              justifyContent: 'space-around',
-              flexDirection: 'column'
-            }}
-            settings={[
-              { label: '0 50:', color: '#EAF19D', key: '0 50' , format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked'},
-              { label: '50 100:', color: '#B8E98E', key: '50 100', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked'},
-              { label: '100 150:', color: '#1B97C1', key: '100 150', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked'},
-              { label: '150 200:', color: '#1C52A3', key: '150 200', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked'},
-              { label: '200 250:', color: '#13267F', key: '200 250', format: value => `${numberFormat(value)}`, position: '_column', type: '_stacked'},
-            ]}
-            label={{ key: 'name'}}
-          />
-        )
-      } 
-    },
-  })
+      }
+    }
+  }
 };
 
 
