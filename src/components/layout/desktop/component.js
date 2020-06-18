ee1f6267223c8d732a5af0f8dddf41c3862902e3
“mluena” <maluenarod@gmail.com>
2020-06-18 13:30:55 +0200

    net-change widget (new style), print report

--- src/components/layout/desktop/component.js
+++ src/components/layout/desktop/component.js
@@ -5,10 +5,27 @@ import Sidebar from 'components/sidebar';
 import logo from 'components/layout/mangrove-logo.svg';
 import styles from '../style.module.scss';
 
-const DesktopLayout = () => (
-  <div>
+const DesktopLayout = ({ location }) => {
+  let stylesOverride = { fontSize: 60, lineHeight: 0.85 };
+  if ((location && location.name.length > 10)) {
+    stylesOverride = { fontSize: 45, lineHeight: 1 };
+  }
+  if ((location && location.name.length > 30)) {
+    stylesOverride = { fontSize: 30, lineHeight: 1 };
+  }
+
+  return (<div>
     <div className={styles.header}>
       <img className={styles.logo} src={logo} alt="mangrove-atlas-logo" />
+      {location && (
+        <h1
+          className={styles.title}
+          style={stylesOverride}
+        >
+          {location.name}
+        </h1>
+
+      )}
     </div>
     <div className={styles.printOnly_wrapper}>
       <Sidebar>
@@ -24,6 +41,7 @@ const DesktopLayout = () => (
       </div>
     </div>
   </div>
-);
+  );
+};
 
 export default DesktopLayout;
