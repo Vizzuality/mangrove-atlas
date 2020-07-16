7bc78220c4506d97c052f693c4b8798e37b359df
“mluena” <maluenarod@gmail.com>
2020-07-16 13:47:38 +0200

    extent widget legend amendments

--- src/modules/widgets/selectors.js
+++ src/modules/widgets/selectors.js
@@ -50,7 +50,7 @@ export const coverageWidget = createSelector(
       percentage: _currentLocation.length_mangrove_m[year] / total * 100,
       unit: '%',
       value: _currentLocation.length_mangrove_m[year],
-      label: `Mangroves in ${year}`
+      label: `Coastline coverage in ${year}`
     }));
 
     return {
