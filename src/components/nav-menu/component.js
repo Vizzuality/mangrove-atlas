a060ff6357f83c657d22dfe77708ff9b0b4f8eda
María Luena Rodríguez <maluenarod@gmail.com>
2020-07-24 02:50:19 +0200

    Fix/map (#224)
    * dates alerts + zoom
    
    * locations ids
    
    * units back to exp in biomass
    
    * dates in dropdown
    
    * hide map controls
    
    * text in widgets
    
    * modal bug fixed
    
    * units changed
    
    * widgets order + units + zoom
    
    * heatmap opacity and colors
    
    * content modal
    
    Co-authored-by: “mluena” <“maluenarod@gmail.com”>

--- src/components/nav-menu/component.js
+++ src/components/nav-menu/component.js
@@ -14,14 +14,21 @@ const NavMenu = ({ fixedHeader }) => {
   const [welcomeContent, toggleMessage] = useState(modalStatus === null);
   const [aboutContent, toggleContent] = useState(false);
 
-  const handleClick = () => {
+  const handleOpen = () => {
+    toggleModal(!isOpen);
+    toggleContent(false);
+  };
+
+  const handleClose = () => {
     myStorage.setItem('modal', false);
     toggleMessage(false);
     toggleModal(!isOpen);
+    toggleContent(false);
   };
 
   const handleContent = () => {
-    toggleContent(!aboutContent);
+    toggleMessage(false);
+    toggleContent(true);
   };
 
   return (
@@ -30,10 +37,10 @@ const NavMenu = ({ fixedHeader }) => {
       [styles.navMenu]: !fixedHeader
     })}
     >
-      <button type="button" onClick={handleClick} />
+      <button type="button" onClick={handleOpen} />
       <Modal
         isOpen={isOpen}
-        onRequestClose={handleClick}
+        onRequestClose={handleClose}
       >
         <div className={styles.modalContent}>
 
@@ -45,8 +52,7 @@ const NavMenu = ({ fixedHeader }) => {
                 Global Mangrove Watch (GMW) is an online platform that provides the remote sensing
                 data and tools for monitoring mangroves necessary for this. It gives universal
                 access to near real-time information on where and what changes there are to mangroves across the world.</p>
-
-              <p>Close this message to continue or find out more about Global Mangrove Watch <button type="button" onClick={handleContent}>here.</button></p>
+              <p onClick={handleContent}>Close this message to continue or find out more about Global Mangrove Watch <span>here.</span></p>
               <a className={styles.welcomeLink} href="http://www.mangrovealliance.org/" target="_blank" rel="noopener noreferrer">Visit Global Mangrove Alliance website</a>
               <HotspotsList />
             </section>
@@ -71,7 +77,7 @@ const NavMenu = ({ fixedHeader }) => {
               <p>Coordinated effort across sectors and geographies will accomplish more, faster. Global Mangrove Watch is the
                 evidence base informing the Global Mangrove Alliance, a collaboration of organisations working to increase
                 the world’s mangrove cover 20% by 2030.
-                <a onClick={handleClick} href="http://www.mangrovealliance.org/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="http://www.mangrovealliance.org/" target="_blank" rel="noopener noreferrer">
                 &nbsp;Learn more at MangroveAlliance.org.
                 </a>
               </p>
@@ -109,28 +115,28 @@ const NavMenu = ({ fixedHeader }) => {
                   </button>
               <nav>
                 Global Mangrove Alliance
-                    <a onClick={handleClick} href="http://www.mangrovealliance.org/mangrove-knowledge/" target="_blank" rel="noopener noreferrer">
+                    <a onClick={handleClose} href="http://www.mangrovealliance.org/mangrove-knowledge/" target="_blank" rel="noopener noreferrer">
                   About
                     </a>
-                <a onClick={handleClick} href="http://www.mangrovealliance.org/mangrove-forests/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="http://www.mangrovealliance.org/mangrove-forests/" target="_blank" rel="noopener noreferrer">
                   Mangroves
                     </a>
-                <a onClick={handleClick} href="hhttp://www.mangrovealliance.org/initiatives/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="hhttp://www.mangrovealliance.org/initiatives/" target="_blank" rel="noopener noreferrer">
                   Initiatives
                     </a>
-                <a onClick={handleClick} href="http://www.mangrovealliance.org/news/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="http://www.mangrovealliance.org/news/" target="_blank" rel="noopener noreferrer">
                   News
                     </a>
-                <a onClick={handleClick} href="http://www.mangrovealliance.org/resources/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="http://www.mangrovealliance.org/resources/" target="_blank" rel="noopener noreferrer">
                   Resources
                     </a>
-                <a onClick={handleClick} href="http://www.mangrovealliance.org/contact/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="http://www.mangrovealliance.org/contact/" target="_blank" rel="noopener noreferrer">
                   Contact
                     </a>
               </nav>
 
               <aside className={styles.footer}>
-                <a onClick={handleClick} href="http://www.mangrovealliance.org/" target="_blank" rel="noopener noreferrer">
+                <a onClick={handleClose} href="http://www.mangrovealliance.org/" target="_blank" rel="noopener noreferrer">
                   <img src={logo} className={styles.logo} alt="Global Mangrove Alliance" />
                 </a>
                 <LanguageSelect />
