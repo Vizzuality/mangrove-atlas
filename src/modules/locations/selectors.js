b846ec3cb16bbb03e37ab2cb7aea7f0521ae0832
David Inga <david.inga@vizzuality.com>
2020-08-07 12:50:15 +0200

    fixed load alerts depending on location_id

--- src/modules/locations/selectors.js
+++ src/modules/locations/selectors.js
@@ -21,6 +21,8 @@ export const currentLocation = createSelector(
       result = _locations.find(location => location.iso === _currentId.iso && location.location_type === 'country');
     } else if (_currentId.id) {
       result = _locations.find(location => location.id === Number(_currentId.id));
+    } if (_currentId.id) {
+      result = _locations.find(location => location.location_id === _currentId.id);
     }
 
     if (!result) return null;
