58af3cd0e239a956f79d9f4643444d31fe3c1e2a
anamontiaga <amontiaga@gmail.com>
2022-01-17 23:22:28 +0100

    fix: real mock data

--- src/widget-components/mangrove-species/config.js
+++ src/widget-components/mangrove-species/config.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 // utils
 import groupBy from 'lodash/groupBy';
-import sortBy from 'lodash/sortBy';
 import { format } from 'd3-format';
 
 // components
@@ -12,27 +11,19 @@ import WidgetLegend from 'components/widget-legend';
 
 import { RED_LIST_CATEGORIES } from './constants';
 
-const numberFormat = format(',.2f');
-
-const COLORS = ['#F9737C', '#7C7C7C', '#F9443E', '#FEA740', '#FCC862'];
+const COLORS = ['#F9737C', '#7C7C7C', '#F9443E', '#FEA740', '#FCC862', '#FCC862'];
 
 const getData = (data) => {
   const { list } = data;
-  if (!list || !list.length) return null;
   const total = 100;
-  return list.map((item, index) => ({
-    value: 70,
+  const { categories } = list;
+  if (!categories || Object.keys(categories).length === 0) return null;
+  return Object.entries(categories).map((item, index) => ({
+    value: item[1],
     color: COLORS[index],
     percentage: 5 / total * 100,
-    label: `${RED_LIST_CATEGORIES[item.red_list_cat]}`
+    label: `${RED_LIST_CATEGORIES[item[0]]}`
   }));
-};
-
-const biomassCoverage = ({ list }, yearSelected) => {
-  const yearData = list.find(d => d.date
-    .includes(yearSelected));
-  if (!yearData) return null;
-  return yearData.agb_mgha_1.toFixed(2);
 };
 
 // const filterData = ({ list }, yearSelected) => sortBy(
@@ -55,42 +46,12 @@ const biomassCoverage = ({ list }, yearSelected) => {
 //   };
 // });
 
-// const getDownloadData = ({ list }) => {
-//   const data = list.filter(l => l.date.includes('2016'));
-//   const total = (Object.values(data[0].toc_hist_tco2eha)
-//     .reduce((previous, current) => current + previous)) / 100;
-
-//   // TO-DO: make dynamic depending on keys
-//   return data.map(l => (
-//     {
-//       Date: l.date,
-//       'Total organic carbon stored in mangroves estimation (tco2e)': l.toc_tco2e,
-//       'Amount stored in above-ground biomass (tco2e)': l.agb_tco2e,
-//       'Amount stored in the upper 1m of soil (tco2e)': l.soc_tco2e,
-//       'Histogram data 0--700': `${l.toc_hist_tco2eha['0--700']}
-//         - color: #EEB66B - percentage (%):
-//         ${(l.toc_hist_tco2eha['0--700']) / total}`,
-//       'Histogram data 700--1400': `${l.toc_hist_tco2eha['700--1400']}
-//         - color: #E68518 - percentage (%):
-//         ${l.toc_hist_tco2eha['700--1400'] / total}`,
-//       'Histogram data 1400-2100': `${l.toc_hist_tco2eha['1400--2100']}
-//         - color: #B84E17 - percentage (%):
-//         ${l.toc_hist_tco2eha['1400--2100'] / total}`,
-//       'Histogram data 2100-2800': `${l.toc_hist_tco2eha['2100--2800']}
-//         - color: #933A06 - percentage (%):
-//         ${l.toc_hist_tco2eha['2100--2800'] / total}`,
-//       'Histogram data 2800-3500': `${l.toc_hist_tco2eha['2800--3500']}
-//         - color: #5C4A3D - percentage (%):
-//         ${l.toc_hist_tco2eha['2800--3500'] / total}`,
-//     }));
-// };
-
 export const CONFIG = {
   parse: (data, yearSelected = 2016) => {
-
+    console.log('--->  DATA', getData(data))
     return {
       chartData: getData(data),
-      coverage: biomassCoverage(data, yearSelected),
+      coverage: null,
       chartConfig: {
         type: 'pie',
         layout: 'centric',
