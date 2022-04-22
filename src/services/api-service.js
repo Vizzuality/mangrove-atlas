3733534e75b4ab6f6b16afdc018cc1eca49904e2
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-04-22 09:22:56 +0200

    changing species endpoint

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 class APIService {
   constructor() {
     this.client = axios.create({
       baseURL: `${process.env.REACT_APP_API_URL}/api`,
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
 
     // staging
     this.clientStaging = axios.create({
       baseURL: `${process.env.REACT_APP_API_URL_STAGING}/api/v2`,
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
   }
 
-  fetchLocations = (params = {}) => this.client
-    .get('/v2//locations', { params })
-    .then((response) => {
+  fetchLocations = (params = {}) =>
+    this.client.get("/v2//locations", { params }).then((response) => {
       const { status, statusText, data } = response;
       if (status >= 400) throw new Error(statusText);
       return data;
@@ -24,7 +23,7 @@ class APIService {
 
   fetchMangroveData = (params = {}) => {
     const { id, iso } = params;
-    const locationParam = id || iso || 'worldwide';
+    const locationParam = id || iso || "worldwide";
 
     return this.client
       .get(`/v1/locations/${locationParam}/mangrove_data`)
@@ -33,33 +32,37 @@ class APIService {
         if (status >= 400) throw new Error(statusText);
         return data;
       });
-  }
+  };
 
   fetchRankingData = (params = {}) => {
-    const { filter = 'gain', startDate = '2007', endDate = '2016', limit = 5 } = params;
+    const {
+      filter = "gain",
+      startDate = "2007",
+      endDate = "2016",
+      limit = 5,
+    } = params;
     return this.client
-      .get(`/v1/locations?rank_by=${filter}_m2&start_date=${startDate}&end_date=${endDate}&location_type=country&limit=${limit}&dir=desc`)
+      .get(
+        `/v1/locations?rank_by=${filter}_m2&start_date=${startDate}&end_date=${endDate}&location_type=country&limit=${limit}&dir=desc`
+      )
       .then((response) => {
         const { status, statusText, data } = response;
         if (status >= 400) throw new Error(statusText);
         return data;
       });
-  }
+  };
 
   fetchMangroveSpeciesData = (params = {}) => {
-    const { locationId = '1_2_74', year = 2016 } = params;
+    const { locationId = "1_2_74", year = 2016 } = params;
 
-    // return this.client
-    // .get(`/v2/widgets/species?&location_id=${locationId}&dir=desc`)
-    // .then((response) => {
-    //   const { status, statusText,
-    //     data
-    //   } = response;
+    return this.client.get(`v2/widgets/biodiversity`).then((response) => {
+      const { status, statusText, data } = response;
 
-    //   const filteredData = data.data.filter(d => d.year === year);
+      const filteredData = data.data.filter((d) => d.year === year);
 
-    //   if (status >= 400) throw new Error(statusText);
-      return ({
+      if (status >= 400) throw new Error(statusText);
+      return filteredData[0];
+      /*   ({
         "data": {
           "location_id": 1136,
           "total": 45,
@@ -77,28 +80,27 @@ class APIService {
         "metadata": {
           "unit": null,
           "note": ''
-        }}) 
-        // || filteredData[0];
-  //   });
-  }
+        }})  */
+      // ||
+    });
+  };
 
   fetchMangroveProtectionData = (params = {}) => {
-    const { locationId = '1_2_74', year = 2016 } = params;
-    return this.client
-    // .get(`/v2/widgets/protected-areas?year=${year}&location_id=${locationId}&dir=desc`)
-    .get(`/v2/widgets/protected-areas?&location_id=${locationId}&dir=desc`)
-    .then((response) => {
-      const { status, statusText,
-        data
-      } = response;
+    const { locationId = "1_2_74", year = 2016 } = params;
+    return (
+      this.client
+        // .get(`/v2/widgets/protected-areas?year=${year}&location_id=${locationId}&dir=desc`)
+        .get(`/v2/widgets/protected-areas?&location_id=${locationId}&dir=desc`)
+        .then((response) => {
+          const { status, statusText, data } = response;
 
-      const filteredData = data.data.filter(d => d.year === year);
+          const filteredData = data.data.filter((d) => d.year === year);
 
-      if (status >= 400) throw new Error(statusText);
-      return filteredData[0];
-    });
-  }
-};
-
+          if (status >= 400) throw new Error(statusText);
+          return filteredData[0];
+        })
+    );
+  };
+}
 
 export default APIService;
