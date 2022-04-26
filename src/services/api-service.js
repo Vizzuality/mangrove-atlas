30cc2aeb20ef8e21bfa25e409d54605fd35788a0
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-04-26 10:37:18 +0200

    adding fake data object

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -61,27 +61,27 @@ class APIService {
       const filteredData = data.data.filter((d) => d.year === year);
 
       if (status >= 400) throw new Error(statusText);
-      return filteredData[0];
-      /*   ({
-        "data": {
-          "location_id": 1136,
-          "total": 45,
-          "endemic": 6,
-          "threatened": 1,
-          "categories": {
-            "cr": 5,
-            "en": 1,
-            "vu": 1,
-            "nt": 4,
-            "lc": 5,
-            "dd": 31,
-          }
+      return;
+      ({
+        data: {
+          location_id: 1136,
+          total: 45,
+          endemic: 6,
+          threatened: 1,
+          categories: {
+            cr: 5,
+            en: 1,
+            vu: 1,
+            nt: 4,
+            lc: 5,
+            dd: 31,
+          },
         },
-        "metadata": {
-          "unit": null,
-          "note": ''
-        }})  */
-      // ||
+        metadata: {
+          unit: null,
+          note: "",
+        },
+      } || filteredData[0]);
     });
   };
 
