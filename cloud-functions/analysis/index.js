2fe137497273e4fccff03dd39c2fc2c50ccde723
David Inga <david.inga@vizzuality.com>
2022-05-04 08:55:36 +0200

    moved cloud functions from data repository

---
+++ cloud-functions/analysis/index.js
@@ -0,0 +1,81 @@
+const ee = require('@google/earthengine');
+const PRIVATE_KEY = require('./credentials.json');
+
+const arrSum = arr => arr.reduce((a, b) => a + b, 0);
+
+const serialize = (originalData) => {
+  if (!originalData || !originalData.length) return null;
+
+  const props = originalData[0].properties;
+  const data = props.histogram;
+  const bucketWidth = data.bucketWidth;
+  const countSum = arrSum(data.histogram);
+
+  return {
+    rows: data.histogram.map((d, i) => ({
+      min: data.bucketMin + (bucketWidth * i),
+      max: data.bucketMin + (bucketWidth * (i + 1)),
+      count: d,
+      percent: d / countSum
+    })),
+    fields: {
+      min: { type: 'number' },
+      max: { type: 'number' },
+      count: { type: 'number' },
+      percent: { type: 'number' }
+    },
+    total_rows: data.histogram.length,
+    stats: {
+       min: props.min,
+      max: props.max,
+      mean: props.mean,
+      stdev: props.stdDev,
+      sum: props.sum
+    }
+  };
+};
+
+const calcHistogram = (assetId, geometry) => {
+  const image = ee.Image(assetId);
+  const reducers = ee.Reducer.histogram(20)
+      .combine(ee.Reducer.minMax(), '', true)
+      .combine(ee.Reducer.mean(),'', true )
+      .combine(ee.Reducer.stdDev(), '', true)
+      .combine(ee.Reducer.sum(), '', true);
+  const regReducer = {
+    collection: ee.FeatureCollection(geometry.features),
+    reducer: reducers
+  };
+  const histogram = image.reduceRegions(regReducer).toList(10000);
+
+  return histogram;
+};
+
+exports.analyse = (req, res) => {
+  const assetId = req.body.assetId;
+  const geometry = req.body.geometry;
+
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  }
+
+  if (!assetId || !geometry) {
+    return res.json({
+      error: 'assetId and geometry are required'
+    });
+  }
+
+  ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
+    ee.initialize(null, null, () => {
+      const result = calcHistogram(assetId, geometry);
+      result.evaluate((json) => res.status(200).json(json));
+    });
+  });
+};
+
---
+++ cloud-functions/fetch-alerts/index.js
@@ -0,0 +1,97 @@
+const { BigQuery } = require('@google-cloud/bigquery');
+const axios = require('axios').default;
+const reverse = require('turf-reverse');
+const http = require('http');
+const https = require('https');
+
+const httpAgent = new http.Agent({keepAlive: true});
+const httpsAgent = new https.Agent({keepAlive: true});
+
+const bigquery = new BigQuery();
+
+const cache = {};
+
+const getLocation = async (locationId) => {
+  if (!locationId) return null;
+  console.log('Getting geometry from locations API');
+  const response = await axios.get(`https://mangrove-atlas-api.herokuapp.com/api/locations/${locationId}`, { httpAgent, httpsAgent });
+  if (response && response.data) return response.data.data;
+  return null;
+};
+
+const makeQuery = (location, startDate, endDate) => {
+  let whereQuery = '';
+
+  if (location) {
+    const geoJSON = {
+      type: 'Feature',
+      properties: {},
+      geometry: location.geometry,
+    };
+    // Reverse coordinates to get [latitude, longitude]
+    const geoJSONreverse = reverse(geoJSON);
+    whereQuery = `AND ST_INTERSECTS(ST_GEOGFROMGEOJSON('${JSON.stringify(geoJSONreverse.geometry)}'), ST_GEOGPOINT(longitude, latitude))`;
+  }
+
+  return `SELECT DATE_TRUNC(scr5_obs_date, MONTH) as date, count(scr5_obs_date) as count
+  FROM deforestation_alerts.alerts
+  WHERE confident = 5
+    AND scr5_obs_date BETWEEN DATE('${startDate}') AND DATE('${endDate}')
+    ${whereQuery}
+  GROUP BY date
+  ORDER BY date ASC`;
+};
+
+/**
+ * Data aggregated by month
+ */
+const alertsJob = async (locationId, startDate = '2020-01-01', endDate = '2020-12-31') => {
+  // First try to get data from cache in order to reduce costs
+  const cacheKey = `${locationId || ''}_${startDate}_${endDate}`;
+  if (cache[cacheKey]) {
+    console.log(`Response from cache ${cacheKey}`);
+    return cache[cacheKey];
+  }
+
+  const location = locationId && await getLocation(locationId);
+  const options = {
+    query: makeQuery(location, startDate, endDate),
+    // Location must match that of the dataset(s) referenced in the query.
+    location: 'US',
+  };
+
+  // Run the query as a job
+  const [job] = await bigquery.createQueryJob(options);
+  console.log(`Job ${job.id} started.`);
+
+  // Wait for the query to finish
+  const [rows] = await job.getQueryResults();
+
+  // Store in cache
+  cache[cacheKey] = rows;
+
+  return rows;
+}
+
+exports.fetchAlerts = (req, res) => {
+  // Get data and return a JSON
+  async function fetch() {
+    const result =  await alertsJob(req.query.location_id, req.query.start_date, req.query.end_date);
+    res.json(result);
+  }
+
+  // Set CORS headers for preflight requests
+  // Allows GETs from any origin with the Content-Type header
+  // and caches preflight response for 3600s
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else {
+    fetch();
+  }
+};
