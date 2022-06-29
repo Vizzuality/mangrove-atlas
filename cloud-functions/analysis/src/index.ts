d514b1b7a7b87d0558bbaf1dfc2c627a159d773c
Alicia <alicia.arenzana@gmail.com>
2022-06-29 17:10:32 +0200

    starting generating analysis endpoint

--- cloud-functions/analysis/src/index.ts
+++ cloud-functions/analysis/src/index.ts
@@ -2,46 +2,9 @@ import{ Request, Response} from 'express';
 import  * as ee from '@google/earthengine'
 import * as PRIVATE_KEY from './credentials.json'
 
-interface AnalysisResponse {
-  rows: {
-    min: number,
-    max: number,
-    count: number,
-    percent: number
-  }[],
-  fields: {
-    min: { type: 'number' },
-    max: { type: 'number' },
-    count: { type: 'number' },
-    percent: { type: 'number' }
-  }
-  total_rows: number,
-  stats: {
-    min: number,
-    max: number,
-    mean: number,
-    stdev: number,
-    sum: number
-  }
-}
-
-interface FeatureCollection {
-  type: string,
-  features: {
-    type: string,
-    properties: {
-      name: string
-    },
-    geometry: {
-      type: string,
-      coordinates: number[]
-    }
-  }[]
-}
-
-interface AnalysisRequestBody {
-  geometry: FeatureCollection
-}
+import { AnalysisResponse } from './AnalysisResponse';
+import { FeatureCollection } from './FeatureCollection';
+import { BlueCarbonCalculations } from './calculations/BlueCarbon';
 
 enum Widgets {
   alpha  = "a",
@@ -52,30 +15,59 @@ interface AnalysisRequestParams {
   widgets: Widgets[],
 }
 
-function isValidAnalysisRequestBody(obj: any): obj is AnalysisRequestBody {
-  return obj.geometry;
+interface AnalysisRequestBody {
+  geometry: FeatureCollection
 }
 
-function isValidAnalysisRequestParams(obj: any): obj is AnalysisRequestParams {
-  return obj.widgets;
-}
+export function analyze(req: Request, res: Response): Response<AnalysisResponse> {
 
-function validate(req: Request, res:  Response): {"status": Boolean, "res": Response} {
-  console.log(req.query)
-  if (!req.body || !req.query) {
-    return {"status": false, "res":  res.status(400).json({"error":"No data provided"})};
+  res.set('Access-Control-Allow-Origin', '*');
+
+  const isValid = validate(req, res);
+
+  if (!isValid.status) {
+    return isValid.res;
   }
 
-  if (!isValidAnalysisRequestBody(req.body)) {
-    return {"status": false, "res":  res.status(400).json({"error":"geometry is required as part of the body"})};
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    return res.status(204).send('');
   }
 
-  if (!isValidAnalysisRequestParams(req.query)) {
-    return {"status": false, "res":  res.status(400).json({"error":"a valid widget param is required"})};
-  }
+  ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
+    ee.initialize(null, null, () => {
+      try {
+        const geometryCollection = ee.FeatureCollection(req.body.geometry);
+        const result = BlueCarbonCalculations.calculateTotalTreeCover(geometryCollection);
 
-  return {"status": true, "res": res};
-}
+        result.evaluate((success, failure) => {
+
+          if (success) {
+            const data = serialize(success);
+            res.status(200).json(data);
+          }
+          if (failure) {
+            console.error(failure)
+            res.status(500).json( {"error": failure});
+          }
+        });
+      }
+      catch (error) {
+        console.error(error)
+        res.status(400).json({"error": error.message});
+      };
+  }, (error) => {
+    console.log(error);
+    res.status(400).json({"error": error});
+  });
+});
+
+  return res;
+
+};
 
 function serialize(originalData: any): AnalysisResponse {
   if (!originalData || !originalData.length) return null;
@@ -112,70 +104,28 @@ function arrSum(arr: []): number {
   return arr.reduce((a, b) => a + b, 0)
 };
 
-const calcHistogram = (assetId, geometry) => {
+function isValidAnalysisRequestBody(obj: any): obj is AnalysisRequestBody {
+  return obj.geometry;
+}
 
-  const image = ee.Image(assetId);
-  const region =ee.FeatureCollection(geometry.features);
-  const reducers = ee.Reducer.histogram(20)
-      .combine(ee.Reducer.minMax(), '', true)
-      .combine(ee.Reducer.mean(),'', true )
-      .combine(ee.Reducer.stdDev(), '', true)
-      .combine(ee.Reducer.sum(), '', true);
+function isValidAnalysisRequestParams(obj: any): obj is AnalysisRequestParams {
+  return obj.widgets;
+}
 
-  const regReducer = {
-    collection: region,
-    reducer: reducers
-  };
-
-  return image.reduceRegions(regReducer).toList(10000);
-};
-
-export function analyze(req: Request, res: Response): Response<AnalysisResponse> {
-
-  res.set('Access-Control-Allow-Origin', '*');
-
-  const isValid = validate(req, res);
-
-  if (!isValid.status) {
-    return isValid.res;
+function validate(req: Request, res:  Response): {"status": Boolean, "res": Response} {
+  console.log(req.query)
+  if (!req.body || !req.query) {
+    return {"status": false, "res":  res.status(400).json({"error":"No data provided"})};
   }
 
-  if (req.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Max-Age', '3600');
-    return res.status(204).send('');
+  if (!isValidAnalysisRequestBody(req.body)) {
+    return {"status": false, "res":  res.status(400).json({"error":"geometry is required as part of the body"})};
   }
 
-  const assetId = 'asdf';
-  const geometry = req.body.geometry;
+  if (!isValidAnalysisRequestParams(req.query)) {
+    return {"status": false, "res":  res.status(400).json({"error":"a valid widget param is required"})};
+  }
 
-  ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
-    ee.initialize(null, null, () => {
-      try {
-        const result = calcHistogram(assetId, geometry);
+  return {"status": true, "res": res};
+}
 
-        result.evaluate((success, failure) => {
-
-          if (success) {
-            const data = serialize(success);
-            res.status(200).json(data);
-          }
-          if (failure) {
-            res.status(500).json( {"error": failure});
-          }
-        });
-      }
-      catch (error) {
-        res.status(400).json({"error": error.message});
-      };
-  }, (error) => {
-    console.log(error);
-    res.status(400).json({"error": error});
-  });
-});
-
-  return res;
-
-};
