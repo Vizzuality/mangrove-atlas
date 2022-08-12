aa27972a459f4bbb73d3ff4d763c2661fb1f5be8
Andrés González <andres.gonzalez@vizzuality.com>
2022-08-12 17:40:42 +0200

    worlwide location env variable

--- src/modules/widgets/constants.js
+++ src/modules/widgets/constants.js
@@ -3,4 +3,5 @@ export const alertsEndDate = { label: '', value: '2022-12-31'};
 
 // ! This ID comes from Mangrove Atlas API. If you heard of a data migration or similar there
 // ! check this value is still valid or weird things will happen (like widgets not loading).
-export const WORLWIDE_LOCATION_ID = 4688;
+// 4688
+export const WORLWIDE_LOCATION_ID = process.env.REACT_APP_WORLWIDE_LOCATION_ID ? +process.env.REACT_APP_WORLWIDE_LOCATION_ID : null ;
