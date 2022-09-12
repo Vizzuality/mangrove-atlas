003390b579ace43e102ccbb8dcfb747907272ec5
mluena <maria.luena@vizzuality.com>
2022-09-12 13:28:32 +0200

    net change new layer

--- src/modules/map-styles/constants.js
+++ src/modules/map-styles/constants.js
@@ -1,20 +1,27 @@
 import { flatten } from "lodash";
 import alerts from "./templates/alerts.json";
 import { extentLayersStyles, extentLayers } from "./templates/extent";
+import restoration from "./templates/restoration.json";
 
 function toRasterSource({ filename, year, source, env }) {
-  if (env === "staging") return {
-    tiles: [`https://mangrove_atlas.storage.googleapis.com/staging/tilesets/${filename}/${year}/{z}/{x}/{y}.png`],
-    type: "raster",
-    tileSize: 256,
-    ...source,
-  };
-  else return {
-    tiles: [`https://mangrove_atlas.storage.googleapis.com/tilesets/${filename}/{z}/{x}/{y}.png`],
-    type: "raster",
-    tileSize: 256,
-    ...source,
-  };
+  if (env === "staging")
+    return {
+      tiles: [
+        `https://mangrove_atlas.storage.googleapis.com/staging/tilesets/${filename}/${year}/{z}/{x}/{y}.png`,
+      ],
+      type: "raster",
+      tileSize: 256,
+      ...source,
+    };
+  else
+    return {
+      tiles: [
+        `https://mangrove_atlas.storage.googleapis.com/tilesets/${filename}/{z}/{x}/{y}.png`,
+      ],
+      type: "raster",
+      tileSize: 256,
+      ...source,
+    };
 }
 
 function createRasterLayer({ name, render }) {
@@ -49,65 +56,55 @@ const vectors = [
       type: "vector",
       promoteId: "ID",
     },
-    layers: [
-      {
-        id: "restoration",
-        type: "fill",
-        source: "restoration",
-        "source-layer": "MOW_Global_Mangrove_Restoration",
-        paint: {
-          "fill-color": "#8122A6",
-          "fill-opacity": [
-            "case",
-            ["boolean", ["feature-state", "hover"], false],
-            0.8,
-            0.5,
-          ],
-          "fill-outline-color": [
-            "case",
-            ["boolean", ["feature-state", "hover"], false],
-            "#8122A6",
-            "rgba(129, 24, 166, 0.5)",
-          ],
-        },
-      },
-    ],
+    layers: restoration,
   },
   {
     id: "extent",
     source: {
       type: "vector",
     },
-    layers: extentLayersStyles
+    layers: extentLayersStyles,
   },
 ];
 
 // years available in google cloud
-const mangroveAbovegroundBiomassYears = [1996, 2007, 2008, 2009, 2010, 2015, 2016, 2017, 2018, 2019, 2020];
-const mangroveCanopyHeightYears = [2007, 2008, 2009, 2010, 2015, 2016, 2017, 2018, 2019, 2020];
+const mangroveAbovegroundBiomassYears = [
+  1996, 2007, 2008, 2009, 2010, 2015, 2016, 2017, 2018, 2019, 2020,
+];
+const mangroveCanopyHeightYears = [
+  2007, 2008, 2009, 2010, 2015, 2016, 2017, 2018, 2019, 2020,
+];
 
-const mangroveAbovegroundBiomassRasters = mangroveAbovegroundBiomassYears.map((year) => ({
-  name: `biomass_1996_v1-0_z0-12_${year}`,
-  filename: "mangrove_aboveground_biomass-v3",
-  year,
-  source: {
-    type: "raster",
-    minzoom: 0,
-    maxzoom: 12,
-  },
-  render: {
-    minzoom: 0,
-    maxzoom: 12,
-  },
-  env: "staging",
-}));
+const mangroveGainLossYears = [
+  2007, 2008, 2009, 2010, 2015, 2016, 2017, 2018, 2019, 2020,
+];
 
-const mangroveAbovegroundBiomassLayers =  mangroveAbovegroundBiomassRasters.map(({ name, year }) => ({
-  layerId: name,
-  year,
-  minZoom: 0,
-  maxZoom: 12,
-}));  
+const mangroveAbovegroundBiomassRasters = mangroveAbovegroundBiomassYears.map(
+  (year) => ({
+    name: `biomass_1996_v1-0_z0-12_${year}`,
+    filename: "mangrove_aboveground_biomass-v3",
+    year,
+    source: {
+      type: "raster",
+      minzoom: 0,
+      maxzoom: 12,
+    },
+    render: {
+      minzoom: 0,
+      maxzoom: 12,
+    },
+    env: "staging",
+  })
+);
+
+const mangroveAbovegroundBiomassLayers = mangroveAbovegroundBiomassRasters.map(
+  ({ name, year }) => ({
+    layerId: name,
+    year,
+    minZoom: 0,
+    maxZoom: 12,
+  })
+);
 
 const mangroveCanopyHeightRasters = mangroveCanopyHeightYears.map((year) => ({
   name: `mangrove_canopy_height-v3_${year}`,
@@ -125,127 +122,75 @@ const mangroveCanopyHeightRasters = mangroveCanopyHeightYears.map((year) => ({
   env: "staging",
 }));
 
-const mangroveCanopyHeightLayers = mangroveCanopyHeightRasters.map(({ name, year }) => ({
-  layerId: name,
-  year,
-  minZoom: 0,
-  maxZoom: 12,
-}));  
+const mangroveCanopyHeightLayers = mangroveCanopyHeightRasters.map(
+  ({ name, year }) => ({
+    layerId: name,
+    year,
+    minZoom: 0,
+    maxZoom: 12,
+  })
+);
 
-const rasters = flatten([
-  ...mangroveAbovegroundBiomassRasters,
-  ...mangroveCanopyHeightRasters,
-  {
-    name: "gl_2007_1996_gain_z0-12",
-    filename: "gl_2007_1996_gain_z0-12",
+const mangroveGainRasters = mangroveGainLossYears.map(
+  (year) => ({
+    name: `gain_${year}`,
+    filename: "gain",
+    year,
     source: {
       type: "raster",
+      minzoom: 0,
+      maxzoom: 12,
+    },
+    render: {
       minzoom: 0,
       maxzoom: 12,
     },
     env: "staging",
-  },
-  {
-    name: "gl_2007_2008_loss_z0-12",
-    filename: "gl_2007_2008_loss_z0-12",
+  })
+);
+
+const mangroveGainLayers = mangroveGainRasters.map(
+  ({ name, year }) => ({
+    layerId: name,
+    year,
+    minZoom: 0,
+    maxZoom: 12,
+  })
+);
+const mangroveLossRasters = mangroveGainLossYears.map(
+  (year) => ({
+    name: `loss_${year}`,
+    filename: "loss",
+    year,
     source: {
       type: "raster",
       minzoom: 0,
       maxzoom: 12,
     },
-    env: "prod",
-  },
-  {
-    name: "gl_2008_2007_gain_z0-12",
-    filename: "gl_2008_2007_gain_z0-12",
-    source: {
-      type: "raster",
+    render: {
       minzoom: 0,
       maxzoom: 12,
     },
-    env: "prod",
-  },
-  {
-    name: "gl_2008_2009_loss_z0-12",
-    filename: "gl_2008_2009_loss_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2009_2008_gain_z0-12",
-    filename: "gl_2009_2008_gain_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2009_2010_loss_z0-12",
-    filename: "gl_2009_2010_loss_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2010_2009_gain_z0-12",
-    filename: "gl_2010_2009_gain_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2010_2015_loss_z0-12",
-    filename: "gl_2010_2015_loss_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2015_2010_gain_z0-12",
-    filename: "gl_2015_2010_gain_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2015_2016_loss_z0-12",
-    filename: "gl_2015_2016_loss_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
-  {
-    name: "gl_2016_2015_gain_z0-12",
-    filename: "gl_2016_2015_gain_z0-12",
-    source: {
-      type: "raster",
-      minzoom: 0,
-      maxzoom: 12,
-    },
-    env: "prod",
-  },
+    env: "staging",
+  })
+);
+
+const mangroveLossLayers = mangroveLossRasters.map(
+  ({ name, year }) => ({
+    layerId: name,
+    year,
+    minZoom: 0,
+    maxZoom: 12,
+  })
+);
+
+const rasters = flatten([
+  ...mangroveAbovegroundBiomassRasters,
+  ...mangroveCanopyHeightRasters,
+  ...mangroveGainRasters,
+  ...mangroveLossRasters
 ]);
+
 const sourcesAndLayers = [...rasters, ...geojsons, ...vectors].reduce(
   (acc, item) => {
     const layers = item.source.type === "raster" ? createRasterLayer(item) : [];
@@ -282,80 +227,7 @@ const layersMap = {
       maxZoom: 12,
     },
   ],
-  net: [
-    {
-      layerId: "gl_1996_2007_loss_z0-12",
-      year: 2007,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2007_1996_gain_z0-12",
-      year: 2007,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2007_2008_loss_z0-12",
-      year: 2008,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2008_2007_gain_z0-12",
-      year: 2008,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2008_2009_loss_z0-12",
-      year: 2009,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2009_2008_gain_z0-12",
-      year: 2009,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2009_2010_loss_z0-12",
-      year: 2010,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2010_2009_gain_z0-12",
-      year: 2010,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2010_2015_loss_z0-12",
-      year: 2015,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2015_2010_gain_z0-12",
-      year: 2015,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2015_2016_loss_z0-12",
-      year: 2016,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-    {
-      layerId: "gl_2016_2015_gain_z0-12",
-      year: 2016,
-      minZoom: 0,
-      maxZoom: 12,
-    },
-  ],
+  net: [ ...mangroveGainRasters, ...mangroveLossLayers],
   "alerts-heat": [
     {
       layerId: "alerts-heat",
