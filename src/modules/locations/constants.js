bf55e832d7b4e7ccbeb04ef50aa8188dfc3c0e2b
mluena <maria.luena@vizzuality.com>
2022-09-15 13:45:31 +0200

    international status fix

--- src/modules/locations/constants.js
+++ src/modules/locations/constants.js
@@ -2,17 +2,17 @@ export const WDPA = {
   DELTA_DU_SALOUM: {
     areaName: 'Delta du Saloum',
     id: '1534',
-    location_id: '2_000000000000000009ed'
+    location_id: process.env.DELTA_DU_SALOUM_LOCATION_ID
   },
   RUFIKI_MAFIA_KILWA: {
     areaName: 'Rufiji-Mafia-Kilwa',
-    id: '1555',
-    location_id: '2_0000000000000000059e'
+    id: '1930',
+    location_id: process.env.RUFIKI_MAFIA_KILWA_LOCATION_ID
   },
   MAFIA_ISLAND: {
     areaName: 'Mafia Island',
     id: '1149',
-    location_id: '2_0000000000000000059e'
+    location_id: process.env.MAFIA_ISLAND_LOCATION_ID
   }
 };
 
