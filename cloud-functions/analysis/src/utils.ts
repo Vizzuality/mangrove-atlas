8c65a3e1583ffbd54f28ffd34ddf291e9b1a647c
David Inga <david.inga@vizzuality.com>
2022-09-23 22:10:09 +0200

    path credentials

--- cloud-functions/analysis/src/utils.ts
+++ cloud-functions/analysis/src/utils.ts
@@ -1,10 +1,14 @@
 import ee from '@google/earthengine';
 import { readFileSync } from 'fs';
+import path from 'path';
 
 export function eeAuthenticate(): Promise<void> {
   return new Promise((resolve, reject) => {
+    console.log('abs path: ', process.cwd());
+    console.log('dirname: ', __dirname);
+    console.log('path result: ', path.resolve(process.cwd(), './credentials.json'));
     // Authenticate to service account using short living access tokens
-    const PRIVATE_KEY = JSON.parse(readFileSync('./credentials.json', 'utf8'));
+    const PRIVATE_KEY = JSON.parse(readFileSync(path.resolve(process.cwd(), './credentials.json'), 'utf8'));
     ee.data.authenticateViaPrivateKey(PRIVATE_KEY,
       () => ee.initialize(null, null, resolve, reject),
       (error) => {
