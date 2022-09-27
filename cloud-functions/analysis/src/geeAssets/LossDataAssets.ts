58f959dd52774189c5f3b18419be6ebfd76bc914
Alicia <alicia.arenzana@gmail.com>
2022-09-27 10:55:18 +0200

    netchange widget ready

--- cloud-functions/analysis/src/geeAssets/LossDataAssets.ts
+++ cloud-functions/analysis/src/geeAssets/LossDataAssets.ts
@@ -2,14 +2,14 @@ import { IDataAsset } from './EEDataset';
 import ee from '@google/earthengine';
 
 export interface ILossDataAsset extends IDataAsset {
-  numYears: number;
+  name: string;
 }
 
 export const LossDataAsset: ILossDataAsset = {
   assetPath: {
     default: "projects/global-mangrove-watch/land-cover/mangrove_extent_loss-v3"
   },
-  numYears: 11,
+  name: "loss",
   getEEAsset(){
     return ee.ImageCollection(this.assetPath.default);
   }
