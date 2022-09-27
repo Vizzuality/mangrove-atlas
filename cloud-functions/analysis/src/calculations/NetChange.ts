c163075c7c0ce7a47a975bc5debf3c31eb574e7a
Alicia <alicia.arenzana@gmail.com>
2022-09-27 10:55:18 +0200

    netchange widget ready

--- cloud-functions/analysis/src/calculations/NetChange.ts
+++ cloud-functions/analysis/src/calculations/NetChange.ts
@@ -10,44 +10,50 @@ class NetChangeCalculationsClass extends BaseCalculation {
 
   calculate(feature: ee.Feature): ee.List {
 
-    const reducers = ee.Reducer.sum();
+    const geometry = feature.geometry();
 
-    const gain = this.Gain.getEEAsset().map(
-      (image: ee.Image) => ee.Feature(null, image.rename('gain'
-      ).multiply(ee.Image.pixelArea()).reduceRegion({
-        reducer: reducers,
-        geometry: feature.geometry(),
-        scale: 30,
-        maxPixels: 1e12,
-        bestEffort: true
-      }))
-    ).toList(10000)
-    const loss = this.Loss.getEEAsset().map(
-      (image: ee.Image) => ee.Feature(null, image.rename('loss'
-      ).multiply(ee.Image.pixelArea()).reduceRegion({
-        reducer: reducers,
-        geometry: feature.geometry(),
-        scale: 30,
-        maxPixels: 1e12,
-        bestEffort: true
-      }))
-    ).toList(10000)
+    const gain = this._getGainLoss(this.Gain, geometry);
+    const loss = this._getGainLoss(this.Loss, geometry);
+    const netChange = this._netChange(gain, loss);
 
     return ee.Dictionary({
-      'data':gain.zip(loss)
-        .map((list: ee.List) => {
-          const item = ee.List(list);
-          const gain = ee.Feature(item.get(0));
-          const loss = ee.Feature(item.get(1));
-          const year = ee.Number.parse(ee.String(loss.id()).slice(8,12));
-          const net_change = ee.Number(gain.get('gain')).subtract(ee.Number(loss.get('loss')));
-          return gain.toDictionary()
-          .combine(loss.toDictionary())
-          .combine(ee.Dictionary({'year':year, 'net_change': net_change}));
-        }),
-  'metadata':{
-
+      'data': netChange,
+      'metadata':{
+        "location_id": "custom-area",
+        "units": {
+          "net_change": "km2",
+          "gain": "km2",
+          "loss": "km2"},
+      'year': netChange.map((f: ee.Dictionary) => ee.Dictionary(f).get('year'))
   }});
+  }
+
+  _getGainLoss(IC: ee.ImageCollection, geom: ee.Geometry): ee.List {
+    const reducers = ee.Reducer.sum();
+    return IC.getEEAsset().map(
+      (image: ee.Image) => ee.Feature(null, image.rename(IC.name)
+                          .multiply(ee.Image.pixelArea()).divide(1000*1000)
+                          .reduceRegion({
+                            reducer: reducers,
+                            geometry: geom,
+                            scale: 30,
+                            maxPixels: 1e12,
+                            bestEffort: true
+                          })))
+              .toList(10000)
+  }
+  _netChange(g: ee.List, l: ee.List): ee.Feature {
+    return g.zip(l).map((list: ee.List) => {
+          const item = ee.List(list);
+          const ga = ee.Feature(item.get(0));
+          const lo = ee.Feature(item.get(1));
+          const year = ee.Number.parse(ee.String(lo.id()).slice(8,12));
+          const net_change = ee.Number(ga.get('gain')).subtract(ee.Number(lo.get('loss')));
+
+          return ga.toDictionary()
+          .combine(lo.toDictionary())
+          .combine(ee.Dictionary({'year':year, 'net_change': net_change}));
+        })
   }
 }
 
