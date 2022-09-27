d2835beb61bc5091a265964723de0736ebd4ed16
Alicia <alicia.arenzana@gmail.com>
2022-09-27 10:55:18 +0200

    netchange widget ready

--- cloud-functions/analysis/src/geeAssets/GainDataAssets.ts
+++ cloud-functions/analysis/src/geeAssets/GainDataAssets.ts
@@ -2,14 +2,14 @@ import { IDataAsset } from './EEDataset';
 import ee from '@google/earthengine';
 
 export interface IGainDataAsset extends IDataAsset {
-  numYears: number;
+  name: string;
 }
 
 export const GainDataAsset: IGainDataAsset = {
   assetPath: {
     default: "projects/global-mangrove-watch/land-cover/mangrove_extent_gain-v3"
   },
-  numYears: 11,
+  name: "gain",
   getEEAsset(){
     return ee.ImageCollection(this.assetPath.default);
   }
