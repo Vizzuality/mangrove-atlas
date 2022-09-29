0107d25aed1a5e47bbeed3561887407ecdd71ef6
mluena <maria.luena@vizzuality.com>
2022-09-29 09:29:03 +0200

    api prod

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class APIService {
   constructor() {
     this.client = axios.create({
-      baseURL: `${process.env.REACT_APP_API_URL_STAGING}/api`,
+      baseURL: `${process.env.REACT_APP_API_URL}/api`,
       headers: { "Content-Type": "application/json" },
     });
   }
