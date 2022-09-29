9572c1a7f79cded617aef90d8e791d6caca600d6
mluena <maria.luena@vizzuality.com>
2022-09-29 10:50:59 +0200

    net change and activity change

--- src/config/data.js
+++ src/config/data.js
@@ -55,7 +55,7 @@ export default {
       name: "Mangrove habitat change",
       slug: "mangrove_activity",
       locationType: ["worldwide"],
-      categoryIds: [],
+      categoryIds: ["all_datasets", "distribution_and_change"],
       layersIds: [],
     },
     {
@@ -82,6 +82,9 @@ export default {
       slug: "mangrove_restoration",
       locationType: ["country", "worldwide"],
       categoryIds: [
+        "all_datasets",
+        "restoration_and_conservation",
+        "ecosystem_services",
       ],
       layersIds: ["restoration"],
     },
@@ -118,7 +121,7 @@ export default {
       name: "Mangrove Emissions Mitigation",
       slug: "mangrove_emissions_mitigation",
       locationType: ["custom", "aoi", "wdpa", "country", "worldwide"],
-      categoryIds: [],
+      categoryIds: ["all_datasets", "climate_and_policy"],
     },
     {
       name: "Mangrove International Status",
