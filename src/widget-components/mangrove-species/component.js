79fe43f1aa4c648d16161dcdbada1da2b5d7b1c7
mluena <maluenarod@gmail.com>
2022-11-28 17:20:52 +0100

    upload file fix rb

--- src/widget-components/mangrove-species/component.js
+++ src/widget-components/mangrove-species/component.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 import ChartWidget from "components/chart-widget";
+import SpeciesLegend from "./species-legend";
 
 import config from "./config";
 
