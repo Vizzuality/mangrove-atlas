b15642095b2a8e27d4fc3dfea8d72c731a50a3b3
mluena <maluenarod@gmail.com>
2023-04-20 15:40:29 +0200

    folder distribution

--- src/containers/datasets/index.tsx
+++ src/containers/datasets/index.tsx
@@ -1,10 +1,10 @@
-import BiomassLayer from 'containers/biomass/layer';
-import BlueCarbonLayer from 'containers/blue-carbon/layer';
-import CountryBoundariesLayer from 'containers/country/layer';
-import HabitatExtentInfo from 'containers/habitat-extent/info.mdx';
-import HabitatExtentLayer from 'containers/habitat-extent/layer';
-import HeightLayer from 'containers/height/layer';
-import ProtectedAreasLayer from 'containers/protected-areas/layer';
+import BiomassLayer from 'containers/datasets/biomass/layer';
+import BlueCarbonLayer from 'containers/datasets/blue-carbon/layer';
+import CountryBoundariesLayer from 'containers/datasets/country/layer';
+import HabitatExtentInfo from 'containers/datasets/habitat-extent/info.mdx';
+import HabitatExtentLayer from 'containers/datasets/habitat-extent/layer';
+import HeightLayer from 'containers/datasets/height/layer';
+import ProtectedAreasLayer from 'containers/datasets/protected-areas/layer';
 
 export const LAYERS = {
   'country-boundaries': CountryBoundariesLayer,
