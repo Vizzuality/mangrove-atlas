82d81f223537f79d496e31ed117445dfee5289b8
anamontiaga <amontiaga@gmail.com>
2023-05-25 18:34:27 +0200

    animate collapsed widgets

--- src/containers/widget/index.tsx
+++ src/containers/widget/index.tsx
@@ -4,7 +4,7 @@ import cn from 'lib/classnames';
 
 import { widgetsCollapsedAtom } from 'store/widgets';
 
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 import WidgetControls from 'components/widget-controls';
@@ -34,39 +34,52 @@ const WidgetWrapper: React.FC<WidgetLayoutProps> = (props: WidgetLayoutProps) =>
     setWidgetsCollapsed(updatedWidgetsCollapsed);
   }, [id, widgetsCollapsed, setWidgetsCollapsed]);
 
-  return (
-    <motion.div
-      animate={{
-        // marginBottom: !widgetsCollapsed[id] ? 0 : '-270px',
-        // marginTop: widgetsCollapsed[id] ? 0 : '-3px',
-        boxShadow: '0px 0px 0px 1px rgba(0, 0, 0, 0.1), 0px 4px 12px rgba(0, 0, 0, 0.08)',
-      }}
-      className={cn({
-        'md:h-fit-conten ml-[3%] mb-3 w-[94%] rounded-2xl border border-[#DADED0] bg-white px-1 py-1 shadow-3xl md:ml-0 md:w-[540px]':
-          true,
-        '-mb-6': !!widgetsCollapsed[id],
-        [className]: !!className,
-      })}
-    >
-      <div
-        className={cn({
-          'rounded-2xl border-2 border-transparent px-9 pt-3 pb-5': true,
-          'border-brand-800': isWidgetActive,
-        })}
-      >
-        <header className="flex items-center justify-between">
-          <h2
-            onClick={handleWidgetCollapsed}
-            className="flex-1 cursor-pointer py-5 text-xs font-bold uppercase -tracking-tighter text-black/85"
-          >
-            {title}
-          </h2>
-          <WidgetControls id={id} />
-        </header>
+  const widgetVariants = {
+    collapsed: {
+      marginBottom: '-24px',
+    },
+    expanded: {
+      marginBottom: '12px',
+    },
+  };
 
-        {!widgetsCollapsed[id] && children}
-      </div>
-    </motion.div>
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={false}
+        variants={widgetVariants}
+        animate={widgetsCollapsed[id] ? 'collapsed' : 'expanded'}
+        exit="expanded"
+        transition={{ type: 'spring', bounce: 0, duration: 0.8 }}
+        className={cn({
+          'md:h-fit-content ml-[3%] w-[94%] rounded-2xl border border-[#DADED0] bg-white px-1 py-1 md:ml-0 md:w-[540px]':
+            true,
+          [className]: !!className,
+        })}
+        style={{
+          boxShadow: 'rgba(0, 0, 0, 0.08) 0px 4px 12px 4px',
+        }}
+      >
+        <div
+          className={cn({
+            'rounded-2xl border-2 border-transparent px-9 pt-3 pb-5': true,
+            'border-brand-800 transition delay-150 ease-in-out': isWidgetActive,
+          })}
+        >
+          <header className="flex items-center justify-between">
+            <h2
+              onClick={handleWidgetCollapsed}
+              className="flex-1 cursor-pointer py-5 text-xs font-bold uppercase -tracking-tighter text-black/85"
+            >
+              {title}
+            </h2>
+            <WidgetControls id={id} />
+          </header>
+
+          {!widgetsCollapsed[id] && children}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
