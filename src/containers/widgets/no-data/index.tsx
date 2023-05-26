84eff9596c59bd6568c64f3e4b815086864a66e7
anamontiaga <amontiaga@gmail.com>
2023-05-26 17:01:15 +0200

    add no data card when there is no widget data

--- src/containers/widgets/no-data/index.tsx
+++ src/containers/widgets/no-data/index.tsx
@@ -1,18 +1,17 @@
 import Icon from 'components/icon';
 
-import NO_DATA_SVG from 'svgs/no-data.svg?sprite';
+import NO_DATA_SVG from 'svgs/ui/no-data.svg?sprite';
 
 const NoData = () => {
   return (
-    <div className="w-[540px]">
-      <div className="m-auto flex min-h-[334px] max-w-full break-inside-avoid flex-col items-center justify-center rounded-[20px] bg-white pt-8 pr-5 pl-10">
-        <Icon className="h-40 w-40" icon={NO_DATA_SVG} />
-        <p>
-          Sorry, there are <b>no data</b> for this location.
-          <br />
-          Try with another category.
-        </p>
-      </div>
+    <div className="m-auto ml-[3%] flex min-h-[334px] w-[94%] max-w-full break-inside-avoid flex-col items-center justify-center rounded-[20px] bg-white pt-8 pr-5 pl-10 shadow-widget md:ml-0 md:w-[540px]">
+      <Icon className="h-40 w-40" icon={NO_DATA_SVG} />
+      {/* md:h-fit-content ml-[3%] w-[94%] rounded-2xl border border-[#DADED0] bg-white px-1 py-1 shadow-widget md:ml-0 md:w-[540px] */}
+      <p className="text-center font-sans text-xs leading-5 sm:text-base sm:leading-6">
+        Sorry, there are <b>no data</b> for this location.
+        <br />
+        Try with another category.
+      </p>
     </div>
   );
 };
