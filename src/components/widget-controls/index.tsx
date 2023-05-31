3c2d53445c4157918a82d5ec7c572defca50ebff
anamontiaga <amontiaga@gmail.com>
2023-05-31 16:02:35 +0200

    update switches

--- src/components/widget-controls/index.tsx
+++ src/components/widget-controls/index.tsx
@@ -26,13 +26,14 @@ const WidgetControls = ({ id }: WidgetControlsType) => {
     const widgetsUpdate = isActive ? activeWidgets.filter((w) => w !== id) : [...activeWidgets, id];
     setActiveWidgets(widgetsUpdate);
   };
+
   return (
     <div className="flex items-center space-x-2">
       {!!download && <Download id={id} />}
       {!!info && <Info id={id} />}
       {!!layer && (
         <SwitchWrapper id={id}>
-          <SwitchRoot onClick={handleClick} defaultChecked={isActive}>
+          <SwitchRoot onClick={handleClick} defaultChecked={isActive} checked={isActive}>
             <SwitchThumb />
           </SwitchRoot>
         </SwitchWrapper>
