01fa8f047bcb1efa0cd6ac86dfa51862da4eb75c
anamontiaga <amontiaga@gmail.com>
2023-06-02 18:11:45 +0200

    details section

---
+++ src/containers/map/restoration-popup/details/index.tsx
@@ -0,0 +1,49 @@
+import cn from 'lib/classnames';
+
+import Detail from 'containers/map/restoration-popup/detail';
+
+const Details = ({ data, isOpen, handleClick }) => {
+  const {
+    Tidal_range,
+    Tidal_range1,
+    Ant_SLR,
+    Ant_SLR1,
+    Future_SLR,
+    Future_SLR1,
+    Time_Loss,
+    Time_Loss1,
+    Flow_Group,
+    Flow_Group1,
+    Med_Patch,
+    Med_Patch1,
+    Contig_Group,
+    Contig_Group1,
+  } = data;
+  return (
+    <div
+      className={cn({
+        'box-border flex w-full cursor-pointer flex-col items-start p-6 font-sans': true,
+        'max-h-[72px] w-full overflow-hidden': !isOpen,
+      })}
+    >
+      <div className="flex w-full items-center justify-between pb-6" onClick={handleClick}>
+        <span className="m-0 text-sm font-semibold">
+          <h3>DETAILS</h3>
+        </span>
+        <span className="text-3xl text-brand-800">{isOpen ? '-' : '+'}</span>
+      </div>
+
+      <div className="flex w-full grow flex-col items-center justify-between">
+        <Detail label="Tidal range" pct={Tidal_range1} value={Tidal_range} />
+        <Detail label="Antecedent SLR" pct={Ant_SLR1} value={Ant_SLR} />
+        <Detail label="Future SLR" pct={Future_SLR1} value={Future_SLR} />
+        <Detail label="Timing of loss" pct={Time_Loss1} value={Time_Loss} />
+        <Detail label="Hydrological disturbance" pct={Flow_Group1} value={Flow_Group} />
+        <Detail label="Patch size and number" pct={Med_Patch1} value={Med_Patch} />
+        <Detail label="Patch connectivity" pct={Contig_Group1} value={Contig_Group} unit="%" />
+      </div>
+    </div>
+  );
+};
+
+export default Details;
--- src/containers/map/restoration-popup/index.tsx
+++ src/containers/map/restoration-popup/index.tsx
@@ -6,6 +6,7 @@ import { restorationPopUpAtom } from 'store/map';
 
 import { useRecoilState } from 'recoil';
 
+import Details from 'containers/map/restoration-popup/details';
 import RestorationScores from 'containers/map/restoration-popup/restoration-scores';
 
 const PopupRestoration = () => {
@@ -52,19 +53,21 @@ const PopupRestoration = () => {
       longitude={popup[0] || null}
       latitude={popup[1] || null}
       onClose={removePopUp}
+      className="c-restoration-popup"
     >
       <div className="w-fit-content h-96">
-        <div className="relative flex w-[440px] max-w-[460px] flex-col items-start bg-white">
+        <div className="relative flex max-w-[460px] flex-col items-start bg-white">
           <RestorationScores
             data={popupInfo}
             isOpen={open === 'restoration'}
             handleClick={() => handleClick('restoration')}
           />
-          {/* <RestorationDetailsInfo
-           data={this.state.popupInfo}
-           isOpen={open === 'details'}
-           handleClick={() => handleClick('details')}
-         />
+          <Details
+            data={popupInfo}
+            isOpen={open === 'details'}
+            handleClick={() => handleClick('details')}
+          />
+          {/*
          <EcosystemServicesInfo
            data={this.state.popupInfo}
            isOpen={open === 'ecosystem'}
