4a1388bcfc88024bbf4180a9c5fb03711276ffb1
mluena <maluenarod@gmail.com>
2023-06-07 08:31:22 +0200

    widget protection layer opacity

--- src/layouts/desktop/index.tsx
+++ src/layouts/desktop/index.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 
-import Blog from 'containers/blog';
 import LanguageSelector from 'containers/language-selector';
 import MapContainer from 'containers/map';
 import Sidebar from 'containers/sidebar';
@@ -11,7 +10,6 @@ import TranslateScripts from 'containers/translate-scripts';
 import WidgetsContainer from 'containers/widgets';
 
 const DesktopLayout = () => {
-  const [blogBanner, setBlogBanner] = useState(true);
   // const MAP_STYLE = useMemo(() => {
   //   return BASEMAPS.find((b) => b.value === basemap)?.url || mapStyle;
   // }, [basemap, mapStyle]);
@@ -51,7 +49,6 @@ const DesktopLayout = () => {
         <MapContainer mapId="default-desktop" />
         <Sidebar />
         <WidgetsContainer />
-        {blogBanner && <Blog setBlogBanner={setBlogBanner} />}
         <LanguageSelector />
       </div>
     </div>
