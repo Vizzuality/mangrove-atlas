b15d9d9520b078a659ef0f0592c9c5cf61c02251
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-06-12 13:44:37 +0200

    map flies to country on click

--- src/containers/datasets/country/layer.tsx
+++ src/containers/datasets/country/layer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 import { Source, Layer } from 'react-map-gl';
 
@@ -6,9 +6,15 @@ import { LayerProps } from 'types/layers';
 
 import { useLayers, useSource } from './hooks';
 
-export const MangrovesCountryBoundariesLayer = ({ beforeId, id }: LayerProps) => {
+export const MangrovesCountryBoundariesLayer = ({ beforeId, id, onAdd, onRemove }: LayerProps) => {
   const SOURCE = useSource();
   const LAYERS = useLayers({ id });
+
+  useEffect(() => {
+    const ids = LAYERS.map((layer) => layer.id);
+    onAdd(ids);
+    return () => onRemove(ids);
+  }, [LAYERS, onAdd, onRemove]);
 
   if (!SOURCE || !LAYERS) return null;
 
