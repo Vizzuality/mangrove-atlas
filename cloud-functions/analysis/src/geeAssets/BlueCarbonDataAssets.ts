601a29222be59fc8de68ac316ae255b982768a11
Alicia <alicia.arenzana@gmail.com>
2023-06-15 09:06:54 +0200

    fixes on the asset

--- cloud-functions/analysis/src/geeAssets/BlueCarbonDataAssets.ts
+++ cloud-functions/analysis/src/geeAssets/BlueCarbonDataAssets.ts
@@ -7,7 +7,7 @@ export interface IHansenDataAsset extends IDataAsset {
 
 export const BlueCarbonDataAsset: IHansenDataAsset = {
   assetPath: {
-    default: "projects/global-mangrove-watch/mangrove-properties/mangrove_blue_carbon-v2_1/2016"
+    default: "projects/global-mangrove-watch/mangrove-properties/mangrove_blue_carbon-v2_1"
   },
   numYears: 1,
   getEEAsset(){
