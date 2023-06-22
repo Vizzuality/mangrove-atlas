d420249999363ba33591354f047aa7dd9988f7b9
Alicia <alicia.arenzana@gmail.com>
2023-06-22 08:40:30 +0200

    improve performance

--- cloud-functions/fetch-alerts-heatmap/index.js
+++ cloud-functions/fetch-alerts-heatmap/index.js
@@ -1,49 +1,17 @@
-const { BigQuery } = require('@google-cloud/bigquery');
-const axios = require('axios').default;
-const reverse = require('turf-reverse');
-const http = require('http');
-const https = require('https');
+const { BigQuery } = require("@google-cloud/bigquery");
+const axios = require("axios").default;
+const reverse = require("turf-reverse");
+const http = require("http");
+const https = require("https");
 
-const httpAgent = new http.Agent({keepAlive: true});
-const httpsAgent = new https.Agent({keepAlive: true});
+const httpAgent = new http.Agent({ keepAlive: true });
+const httpsAgent = new https.Agent({ keepAlive: true });
 
 const bigquery = new BigQuery();
 const cache = {};
 
-const getLocation = async (locationId) => {
-  if (!locationId) return null;
-  console.log('Getting geometry from locations API');
-  const response = await axios.get(`https://mangrove-atlas-api.herokuapp.com/api/v2/locations/${locationId}`, { httpAgent, httpsAgent});
-  if (response && response.data) return response.data.data;
-  return null;
-};
-
-const makeQuery = (location, startDate, endDate) => {
-  let query;
-
-  if (location) {
-    const geoJSON = {
-      type: 'Feature',
-      properties: {},
-      geometry: location.geometry,
-    };
-    // Reverse coordinates to get [latitude, longitude]
-    const geoJSONreverse = reverse(geoJSON);
-    const whereQuery = `AND ST_INTERSECTS(ST_GEOGFROMGEOJSON('${JSON.stringify(geoJSONreverse.geometry)}'), ST_GEOGPOINT(longitude, latitude))`;
-
-    query = `WITH a AS (
-      SELECT latitude, longitude, count(ST_GEOGPOINT(longitude, latitude)) AS count
-      FROM deforestation_alerts.alerts
-      WHERE confident = 5
-        AND scr5_obs_date >= '${startDate}'
-        AND scr5_obs_date <= '${endDate}'
-        ${whereQuery}
-      GROUP BY latitude, longitude
-    )
-    SELECT latitude, longitude, count, MIN(count) OVER() AS min, MAX(count) OVER() AS max
-    FROM a`;
-  } else {
-    query = `WITH a AS (
+const makeQuery = (startDate, endDate) => {
+  const query = `WITH a AS (
       SELECT TRUNC(longitude, 2) AS longitude,
         TRUNC(latitude, 2) AS latitude,
       FROM deforestation_alerts.alerts
@@ -57,22 +25,21 @@ const makeQuery = (location, startDate, endDate) => {
     )
     SELECT latitude, longitude, count, MIN(count) OVER() AS min, MAX(count) OVER() AS max
     FROM b`;
-  }
 
   return query;
 };
 
 const serializeToGeoJSON = (data) => ({
-  type: 'FeatureCollection',
-  name: 'deforestation-alerts',
+  type: "FeatureCollection",
+  name: "deforestation-alerts",
   features: data.map((d) => ({
-    type: 'Feature',
+    type: "Feature",
     properties: {
       count: d.count,
       intensity: d.count / d.max,
     },
     geometry: {
-      type: 'Point',
+      type: "Point",
       coordinates: [d.longitude, d.latitude],
     },
   })),
@@ -84,23 +51,25 @@ const serializeToGeoJSON = (data) => ({
 
 /**
  * Data aggregated by month, latitude and longitude
- * @param {ID} locationId
+ * @param {string} locationId # this is no longer used
  * @param {Date} startDate
  * @param {Date} endDate
  */
-const alertsJob = async (locationId, startDate = '2020-01-01', endDate = '2020-12-31') => {
+const alertsJob = async (
+  locationId,
+  startDate = "2020-01-01",
+  endDate = new Date().toISOString().split("T")[0]
+) => {
   // First try to get data from cache in order to reduce costs
-  const cacheKey = `${locationId || ''}_${startDate}_${endDate}`;
+  const cacheKey = `_${startDate}_${endDate}`;
   if (cache[cacheKey]) {
     console.log(`Rensponse from cache ${cacheKey}`);
     return cache[cacheKey];
   }
-
-  const location = await getLocation(locationId);
   const options = {
-    query: makeQuery(location, startDate, endDate),
+    query: makeQuery(startDate, endDate),
     // Location must match that of the dataset(s) referenced in the query.
-    location: 'US',
+    location: "global",
   };
 
   // Run the query as a job
@@ -121,21 +90,21 @@ exports.fetchAlertsHeatmap = (req, res) => {
   // Get data and return a JSON
   async function fetch() {
     const { location_id, start_date, end_date } = req.query;
-    const result =  await alertsJob(location_id, start_date, end_date);
+    const result = await alertsJob(location_id, start_date, end_date);
     res.json(result);
   }
 
   // Set CORS headers for preflight requests
   // Allows GETs from any origin with the Content-Type header
   // and caches preflight response for 3600s
-  res.set('Access-Control-Allow-Origin', '*');
+  res.set("Access-Control-Allow-Origin", "*");
 
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Max-Age', '3600');
-    res.status(204).send('');
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
   } else {
     fetch();
   }
