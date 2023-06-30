266d7ecdd813dc12ca87ab5074a2776b779e8c86
mluena <maluenarod@gmail.com>
2023-06-30 13:09:27 +0200

    data and clud functions update

--- cloud-functions/analysis/src/geeAssets/BlueCarbonDataAssets.ts
+++ cloud-functions/analysis/src/geeAssets/BlueCarbonDataAssets.ts
@@ -7,7 +7,7 @@ export interface IHansenDataAsset extends IDataAsset {
 
 export const BlueCarbonDataAsset: IHansenDataAsset = {
   assetPath: {
-    default: "projects/global-mangrove-watch/mangrove-properties/mangrove_total_co2e_1996--2016"
+    default: "projects/global-mangrove-watch/mangrove-properties/mangrove_blue_carbon-v2_1"
   },
   numYears: 1,
   getEEAsset(){
