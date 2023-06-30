376d9cf4c1e5a5a1e70856c774d9277cb19981b7
anamontiaga <amontiaga@gmail.com>
2023-06-30 12:07:56 +0200

    guide text dark basemap

--- src/containers/map/index.tsx
+++ src/containers/map/index.tsx
@@ -112,6 +112,8 @@ const MapContainer = ({ mapId }: { mapId: string }) => {
 
   const { [mapId]: map } = useMap();
 
+  /// console.log('------>', map.getStyle().layers);
+
   const {
     query: { params },
     push,
