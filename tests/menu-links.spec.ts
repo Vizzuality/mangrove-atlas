50a19e9af5fb8ff9aa3d5f5d082fc47f0970650e
Maria Sola <maria.sola@vizzuality.com>
2023-07-14 13:03:54 +0200

    clean code and variables

--- tests/menu-links.spec.ts
+++ tests/menu-links.spec.ts
@@ -29,22 +29,28 @@ const links = [
 
 test('test menu links', async ({ page }) => {
   await page.goto('/');
-
-  await page.getByTestId('menu-button').click();
-  await page.getByRole('button', { name: 'About this tool' }).click();
-  await page.getByRole('heading', { name: 'About Global Mangrove Watch' }).click();
   await page.getByRole('button', { name: 'Close' }).click();
 
-  await page.getByTestId('menu-button').click();
-  await page.getByRole('button', { name: 'News' }).click();
-  await page.getByRole('button', { name: 'Close' }).nth(1).click();
+  const menuButton = page.getByTestId('menu-button');
+  await menuButton.click();
 
-  await page.getByTestId('menu-button').click();
-  await page.getByRole('button', { name: 'Global Mangrove Alliance' }).click();
+  const menuContent = page.getByTestId('menu-content');
+  await menuContent.isVisible();
+
+  await menuContent.getByRole('button', { name: 'About this tool' }).click();
+  await menuContent.getByRole('heading', { name: 'About Global Mangrove Watch' }).click();
+  await menuContent.getByRole('button', { name: 'Close' }).click();
+
+  await menuButton.click();
+  await menuContent.getByRole('button', { name: 'News' }).click();
+  await menuContent.getByRole('button', { name: 'Close' }).nth(1).click();
+
+  await menuButton.click();
+  await menuContent.getByRole('button', { name: 'Global Mangrove Alliance' }).click();
   const pageOpenPromise = page.waitForEvent('popup');
 
   for (const link of links) {
-    await page.getByRole('link', { name: link.name }).click();
+    await menuContent.getByRole('link', { name: link.name }).click();
     const promise = await pageOpenPromise;
     await promise.goto(link.link);
   }
