c36a0a20d8f1406ec94e12ecf865e40c5364f36b
mluena <maluenarod@gmail.com>
2023-07-20 17:29:44 +0200

    hidden widgets

--- src/containers/sidebar/menu/index.tsx
+++ src/containers/sidebar/menu/index.tsx
@@ -151,7 +151,7 @@ const Menu = () => {
                   </motion.div>
                 )}
               </AnimatePresence>
-              {process.env.NEXT_PUBLIC_FEATURE_FLAG_WIDGETS && (
+              {/* {process.env.NEXT_PUBLIC_FEATURE_FLAG_WIDGETS && (
                 <AnimatePresence>
                   {section === 'news' && (
                     <motion.div
@@ -168,7 +168,7 @@ const Menu = () => {
                     </motion.div>
                   )}
                 </AnimatePresence>
-              )}
+              )} */}
               <DialogClose />
             </DialogContent>
           </Dialog>
