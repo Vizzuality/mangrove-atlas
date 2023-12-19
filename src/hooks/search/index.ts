30486bb49e3f236c7ded7e601a38b13bc19eddd2
anamontiaga <amontiaga@gmail.com>
2023-12-19 18:21:05 +0100

    custom remove accents when search location

--- src/hooks/search/index.ts
+++ src/hooks/search/index.ts
@@ -1,12 +1,19 @@
 import { useMemo } from 'react';
+
 import diacritics from 'diacritics';
 import Fuse from 'fuse.js';
 
+type ItemWithAccents<T> = T & { name: string };
+
 export const useSearch = <T = unknown>(items: T[], search: string, fields: string[]) => {
+  const itemsNamesWithoutAccents: ItemWithAccents<T>[] = items.map((item: ItemWithAccents<T>) => ({
+    ...item,
+    name: item.name ? item.name.normalize('NFD').replace(/[\u0300-\u036F]/g, '') : '',
+  }));
   const fuse =
     search &&
     search.length &&
-    (new Fuse(items, {
+    (new Fuse(itemsNamesWithoutAccents, {
       keys: fields,
       shouldSort: true,
       includeMatches: true,
