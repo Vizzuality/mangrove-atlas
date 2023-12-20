1bb25dbd10b5dd49c4b44365c5fbe624353d146a
anamontiaga <amontiaga@gmail.com>
2023-12-20 12:07:38 +0100

    remove threshold for perfect match

--- src/hooks/search/index.ts
+++ src/hooks/search/index.ts
@@ -3,21 +3,14 @@ import { useMemo } from 'react';
 import diacritics from 'diacritics';
 import Fuse from 'fuse.js';
 
-type ItemWithAccents<T> = T & { name: string };
-
 export const useSearch = <T = unknown>(items: T[], search: string, fields: string[]) => {
-  const itemsNamesWithoutAccents: ItemWithAccents<T>[] = items.map((item: ItemWithAccents<T>) => ({
-    ...item,
-    name: item.name ? item.name.normalize('NFD').replace(/[\u0300-\u036F]/g, '') : '',
-  }));
   const fuse =
     search &&
     search.length &&
-    (new Fuse(itemsNamesWithoutAccents, {
+    (new Fuse(items, {
       keys: fields,
       shouldSort: true,
       includeMatches: true,
-      threshold: 0.1,
       location: 0,
       distance: 300,
       minMatchCharLength: 1,
@@ -25,6 +18,7 @@ export const useSearch = <T = unknown>(items: T[], search: string, fields: strin
 
   return useMemo(
     () => fuse && fuse.search(diacritics.remove(search)).map((d) => d.item),
+
     [search, fuse]
   ) satisfies T[];
 };
