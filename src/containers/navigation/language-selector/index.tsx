f1670dca139711729603104ec2790bb04d6d3aa3
mluena <maluenarod@gmail.com>
2023-12-28 14:49:53 +0100

    custom language selector

--- src/containers/navigation/language-selector/index.tsx
+++ src/containers/navigation/language-selector/index.tsx
@@ -1,10 +1,41 @@
+import { useCallback, useState, MouseEvent } from 'react';
+
 import Helper from 'containers/guide/helper';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from 'components/dropdown';
 import Icon from 'components/icon';
 
 import TRANSLATE_SVG from 'svgs/tools-bar/translate.svg?sprite';
 
+interface Transifex {
+  live: {
+    detectLanguage: () => string;
+    getAllLanguages: () => { code: string; name: string }[];
+    translateTo: (string) => string;
+  };
+}
+
 const LanguageSelector = () => {
+  const Transifex = window?.Transifex as Transifex;
+  const locale = Transifex.live.detectLanguage();
+  const languages = Transifex.live.getAllLanguages() satisfies { code: string; name: string }[];
+  const defaultLanguage = languages?.find((lang) => lang.code === locale).name;
+
+  const [currentLanguage, setCurrentLanguage] = useState(defaultLanguage);
+
+  const handleChange = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      Transifex.live.translateTo(e.currentTarget.value);
+      setCurrentLanguage(e.currentTarget.id);
+    },
+    [Transifex]
+  );
+
   return (
     <Helper
       className={{
@@ -12,20 +43,38 @@ const LanguageSelector = () => {
         tooltip: 'w-fit-content',
       }}
       tooltipPosition={{ top: -10, left: -60 }}
-      message="main LanguageSelector"
+      message="Choose your preferred language here. There is a choice between English, French and Spanish"
     >
-      <button
-        data-testid="LanguageSelector-button"
-        type="button"
-        className="relative flex items-center space-x-2"
-      >
-        <Icon
-          icon={TRANSLATE_SVG}
-          className="h-6 w-6 stroke-white"
-          description="language-selection"
-        />
-        <span className="font-sans text-sm text-white">English</span>
-      </button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="flex items-center space-x-2">
+            <Icon
+              icon={TRANSLATE_SVG}
+              className="h-6 w-6 stroke-white"
+              description="language-selection"
+            />
+            <span className="text-sm text-white">{currentLanguage}</span>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-brand-800">
+          {languages.map((lang: { code: string; name: string }) => (
+            <DropdownMenuItem key={lang.code} asChild>
+              <button
+                data-testid={`${lang.code}-button`}
+                id={lang.name}
+                value={lang.code}
+                type="button"
+                className="cursor-pointer hover:bg-white"
+                onClick={handleChange}
+              >
+                <span className="font-sans text-sm text-white hover:text-brand-800">
+                  {lang.name}
+                </span>
+              </button>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Helper>
   );
 };
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,7 @@ declare global {
   interface Window {
     // ? As we are using explicitily window to access the `gtag` property we need to declare it before using it
     gtag: UniversalAnalytics.ga;
+    Transifex: Transifex;
   }
 }
 
