f0c09f11f9372497432de8dee44601f36071e22c
anamontiaga <amontiaga@gmail.com>
2024-01-16 14:49:14 +0100

    fix sort legend

--- src/containers/map/legend/index.tsx
+++ src/containers/map/legend/index.tsx
@@ -10,6 +10,7 @@ import { FaArrowDown, FaArrowUp } from 'react-icons/fa6';
 import { useRecoilState } from 'recoil';
 
 import SortableList from 'components/map/legend/sortable/list';
+import { ActiveLayers } from 'types/layers';
 
 import LegendItem from './item';
 
@@ -17,7 +18,6 @@ const Legend = ({ embedded = false }: { embedded?: boolean }) => {
   const [activeLayers, setActiveLayers] = useRecoilState(activeLayersAtom);
 
   const [isOpen, setIsOpen] = useState(false);
-  const [sortArray, setSortArray] = useState([]);
 
   const openLegend = useCallback(() => {
     if (!!activeLayers.length) setIsOpen(true);
@@ -27,15 +27,15 @@ const Legend = ({ embedded = false }: { embedded?: boolean }) => {
     setIsOpen(false);
   }, []);
 
-  const onChangeOrder = useCallback(
-    (ids: string[]) => {
-      setSortArray(ids);
-      const sortedLayers = activeLayers.sort(
-        (a, b) => sortArray.indexOf(a.id) - sortArray.indexOf(b.id)
-      );
-      setActiveLayers(sortedLayers);
+  const handleChangeOrder = useCallback(
+    (order: string[]) => {
+      const newLayers = order.map((id) => {
+        return activeLayers.find((la) => la.id === id);
+      }) as ActiveLayers[];
+
+      setActiveLayers(newLayers);
     },
-    [activeLayers, sortArray, setActiveLayers]
+    [activeLayers, setActiveLayers]
   );
 
   const contentVariants = {
@@ -78,7 +78,7 @@ const Legend = ({ embedded = false }: { embedded?: boolean }) => {
               <div className="w-[360px] gap-4 rounded-3xl border bg-white shadow-medium animate-in duration-300 data-[state=open]:fade-in-60 data-[state=close]:slide-in-from-bottom-0 data-[state=open]:slide-in-from-bottom-16">
                 <div className="divide-black/42 box-content flex flex-col space-y-1 divide-y overflow-y-auto px-4 pt-4 md:max-h-[55vh] md:print:hidden">
                   <SortableList
-                    onChangeOrder={onChangeOrder}
+                    onChangeOrder={handleChangeOrder}
                     sortable={{ handle: true, enabled: true }}
                   >
                     {activeLayers.map((l) => {
