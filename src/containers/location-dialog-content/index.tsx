3aa0a034948b42e1a1f454204a65cb2922920e93
anamontiaga <amontiaga@gmail.com>
2024-01-17 19:26:25 +0100

    fix locations

--- src/containers/location-dialog-content/index.tsx
+++ src/containers/location-dialog-content/index.tsx
@@ -1,12 +1,11 @@
 import LocationsList from 'containers/locations-list';
 
 import { DialogContent, DialogClose } from 'components/dialog';
-import { LOCATIONS_DIALOG_STYLES } from 'styles/locations';
 
 const LocationDialogContent = ({ close }: { close: () => void }) => {
   return (
     <DialogContent
-      className={`${LOCATIONS_DIALOG_STYLES} h-[90vh] w-[540px]`}
+      className="!w-[540px]"
       onEscapeKeyDown={close}
       onInteractOutside={close}
       data-testid="location-dialog-content"
--- src/styles/locations.js
+++
@@ -1,2 +0,0 @@
-// ensures dialog have the same styles regardless of where it is triggered
-export const LOCATIONS_DIALOG_STYLES = 'rounded-3xl px-10 pt-10 pb-0';
