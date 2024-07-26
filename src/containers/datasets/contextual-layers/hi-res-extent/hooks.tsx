cdee3f5fe859000623d99a38d5a689cd547a0b91
María <maluenarod@gmail.com>
2024-07-26 07:04:25 +0200

    hi res last version data layer

--- src/containers/datasets/contextual-layers/hi-res-extent/hooks.tsx
+++ src/containers/datasets/contextual-layers/hi-res-extent/hooks.tsx
@@ -6,7 +6,7 @@ export function useSource(): SourceProps {
   return {
     id: 'hi-res-extent',
     type: 'vector',
-    url: 'mapbox://globalmangrovewatch.gmw_v4_sen2_sgl_cls_v007_all_z',
+    url: 'mapbox://globalmangrovewatch.gmw_v4019_sen2_mng_all_zooms',
   };
 }
 const darkTeal = '#005f73';
@@ -17,7 +17,7 @@ export function useLayers({ id }: { id: LayerProps['id'] }): LayerProps[] {
         id: `${id}_fill`,
         type: 'fill',
         source: 'hi-res-extent',
-        'source-layer': 'gmw_v4_sen2_sgl_cls_v007_wgs84',
+        'source-layer': 'gmw_v4019_sen2_mng',
         paint: {
           'fill-color': darkTeal,
           'fill-opacity': ['interpolate', ['linear'], ['zoom'], 0, 0.5, 12, 0.3],
@@ -30,7 +30,7 @@ export function useLayers({ id }: { id: LayerProps['id'] }): LayerProps[] {
         id: `${id}_line`,
         type: 'line',
         source: 'hi-res-extent',
-        'source-layer': 'gmw_v4_sen2_sgl_cls_v007_wgs84',
+        'source-layer': 'gmw_v4019_sen2_mng',
         paint: {
           'line-color': darkTeal,
           'line-opacity': 0.8,
