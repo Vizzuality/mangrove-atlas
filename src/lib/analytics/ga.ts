4ed7dc2825e57b83f1200e338b08cb394b475861
María <maluenarod@gmail.com>
2025-07-16 09:37:15 +0200

    ga events

--- src/lib/analytics/ga.ts
+++ src/lib/analytics/ga.ts
@@ -1,4 +1,14 @@
+import ReactGA from 'react-ga4';
+
+
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+type ReactGAEvent = {
+  action: string;
+  label?: string;
+  value?: number;
+  nonInteraction?: boolean;
+};
 
 // log the pageview with their URL
 export const GAPage = (url: string): void => {
@@ -15,3 +25,10 @@ export const GAEvent = ({ action, params }): void => {
     window.gtag('event', action, params);
   }
 };
+
+// Track events with a predefined category
+export function trackEvent(categoryName: string) {
+  return (options: ReactGAEvent) => {
+    ReactGA.event({ category: categoryName, ...options });
+  };
+}
