0f64d0c18fc0270780348ed44a12328494aabf90
María <maluenarod@gmail.com>
2025-07-22 11:37:17 +0200

    legend to fisheries widget

--- src/containers/widgets/widgets-menu/index.tsx
+++ src/containers/widgets/widgets-menu/index.tsx
@@ -67,7 +67,6 @@ const WidgetsMenu: FC = () => {
 
   const handleWidgets = useCallback(
     (e) => {
-<<<<<<< HEAD
       const isActive = activeWidgets.includes(e);
 
       // Google Analytics tracking
@@ -76,11 +75,6 @@ const WidgetsMenu: FC = () => {
           action: 'activate widget',
           label: `Widgets deck tool - widget -${e}`,
         });
-=======
-      const isActive = activeWidgets.includes(e); 
-      if (!isActive) {
-        trackEvent(e);
->>>>>>> 4ed7dc28 (ga events)
       }
       const updatedWidgets = isActive
         ? activeWidgets.filter((widget) => widget !== e)
--- src/lib/analytics/ga.ts
+++ src/lib/analytics/ga.ts
@@ -1,9 +1,5 @@
 import ReactGA from 'react-ga4';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 4ed7dc28 (ga events)
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // log the pageview with their URL
@@ -22,16 +18,7 @@ export const GAEvent = ({ action, params }): void => {
   }
 };
 
-<<<<<<< HEAD
 // Track events
 export function trackEvent(eventName: string, params?: Record<string, any>): void {
   ReactGA.event(eventName, params);
 }
-=======
-// Track events with a predefined category
-export function trackEvent(categoryName: string) {
-  return (options: ReactGAEvent) => {
-    ReactGA.event({ category: categoryName, ...options });
-  };
-}
->>>>>>> 4ed7dc28 (ga events)
