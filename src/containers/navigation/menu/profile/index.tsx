06b3c47dab7e6f7f58fbd0b23a43a2f5e97f48c9
María <maluenarod@gmail.com>
2026-03-10 18:11:25 +0100

    news dot update

--- src/containers/navigation/menu/profile/index.tsx
+++ src/containers/navigation/menu/profile/index.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import Link from 'next/link';
-
-import cn from '@/lib/classnames';
-
-import { TAB_TRIGGER_STYLE, Tabs, TabsContent, TabsList, TabsTrigger } from 'components/ui/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from 'components/ui/tabs';
 
 import AccountContent from './account';
 import SavedAreasContent from './saved-areas';
@@ -13,9 +9,9 @@ import SubscriptionsContent from './subscriptions';
 const Profile = () => {
   return (
     <div className="space-y-6">
-      <h2 className="text-3xl leading-[32px] font-light text-black/85">My profile</h2>
+      <h2 className="text-3xl leading-8 font-light text-black/85">My profile</h2>
 
-      <Tabs defaultValue="account" className="min-w-[476px] space-y-6">
+      <Tabs defaultValue="account" className="min-w-119 space-y-6">
         <TabsList className="w-full gap-2">
           <TabsTrigger value="account" className="flex-1">
             Account
@@ -26,16 +22,6 @@ const Profile = () => {
           <TabsTrigger value="saved-areas" className="flex-1">
             Saved areas
           </TabsTrigger>
-          <Link
-            href={
-              process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-                ? process.env.NEXT_PUBLIC_MRTT_SITE_PROD
-                : process.env.NEXT_PUBLIC_MRTT_SITE_STAGING
-            }
-            className={cn(TAB_TRIGGER_STYLE, 'flex-1')}
-          >
-            MRTT
-          </Link>
         </TabsList>
         <TabsContent value="account">
           <AccountContent />
