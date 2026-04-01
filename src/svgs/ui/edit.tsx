c94238e0a9fc278cd6b87bf19c42bb462974e40f
María <maluenarod@gmail.com>
2026-04-01 12:59:17 +0200

    svgs

--- src/svgs/ui/edit.tsx
+++ src/svgs/ui/edit.tsx
@@ -4,6 +4,7 @@ interface SVGRProps {
   title?: string;
   titleId?: string;
 }
+
 const SvgEdit = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
